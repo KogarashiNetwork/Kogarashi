35e913a9bde548a1e2124a43b1774c6614629dfe
NoCtrlZ <phantomofrotten@gmail.com>
2021-12-26 14:32:39 +0900

    add mul method

--- core/eddy/src/fr.rs
+++ core/eddy/src/fr.rs
@@ -1,12 +1,14 @@
-use crate::arithmetic::{add, double, sub};
+use crate::arithmetic::{add, double, mul, sub};
 use parity_scale_codec::{Decode, Encode};
 
-pub const MODULUS: &[u64; 4] = &[
+pub(crate) const MODULUS: &[u64; 4] = &[
     0xd0970e5ed6f72cb7,
     0xa6682093ccc81082,
     0x06673b0101343b00,
     0x0e7db4ea6533afa9,
 ];
+
+pub(crate) const INV: u64 = 0x1ba3a358ef788ef9;
 
 #[derive(Debug, Clone, Decode, Encode)]
 pub(crate) struct Fr(pub(crate) [u64; 4]);
@@ -22,5 +24,9 @@ impl Fr {
 
     pub fn double_assign(&mut self) {
         self.0 = double(&self.0)
+    }
+
+    pub fn mul_assign(&mut self, other: Self) {
+        self.0 = mul(&self.0, &other.0)
     }
 }
