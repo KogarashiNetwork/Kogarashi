d1400cc242362d57def548ff1511f307c7607e51
NoCtrlZ <phantomofrotten@gmail.com>
2021-12-28 20:51:47 +0900

    need to fix random test

--- core/jubjub/src/fr.rs
+++ core/jubjub/src/fr.rs
@@ -203,6 +203,23 @@ impl Display for Fr {
 #[cfg(test)]
 mod fr_tests {
     use super::*;
+    use rand::SeedableRng;
+    use rand_xorshift::XorShiftRng;
+
+    #[test]
+    fn test_random() {
+        for i in 0..10000 {
+            let mut initial_seeds = [
+                0x43, 0x62, 0xbe, 0x7d, 0x23, 0xad, 0x56, 0xcd, 0x33, 0x0a, 0x22, 0x23, 0x46, 0x36,
+                0xac, 0xef,
+            ];
+            let seed = i as u8 % u8::MAX;
+            let index = (seed % 16) as usize;
+            initial_seeds[index] = seed;
+            let rng = XorShiftRng::from_seed(initial_seeds);
+            Fr::random(rng).unwrap();
+        }
+    }
 
     #[test]
     fn test_from_hex() {
