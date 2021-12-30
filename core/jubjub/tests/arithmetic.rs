2daa235d93a60bb74fb14fb02aac330e792597ca
NoCtrlZ <phantomofrotten@gmail.com>
2021-12-30 19:26:30 +0900

    sub integration test

--- core/jubjub/tests/arithmetic.rs
+++ core/jubjub/tests/arithmetic.rs
@@ -26,4 +26,28 @@ mod arithmetic_tests {
             assert_eq!(a, c);
         }
     }
+
+    #[test]
+    fn sub_test() {
+        for i in 0..100000 {
+            let mut initial_seeds = [
+                0x43, 0x62, 0xbe, 0x7d, 0x23, 0xad, 0x56, 0xcd, 0x33, 0x0a, 0x22, 0x23, 0x46, 0x36,
+                0xac, 0xef,
+            ];
+            let seed = i as u8 % u8::MAX;
+            let index = (seed % 16) as usize;
+            initial_seeds[index] = seed;
+            let rng = XorShiftRng::from_seed(initial_seeds);
+
+            let mut a = Fr::random(rng);
+            let b = a.clone();
+            let mut c = a.clone();
+            let mut d = a.clone();
+            a.sub_assign(b);
+            c.double_assign();
+            d.double_assign();
+            c.sub_assign(d);
+            assert_eq!(a, c);
+        }
+    }
 }
