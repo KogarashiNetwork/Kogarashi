d6b40d213089fc98ebcf1ee1ae54bf9dbf473ebf
NoCtrlZ <phantomofrotten@gmail.com>
2021-12-30 19:35:31 +0900

    need to fix mul test

--- core/jubjub/tests/arithmetic.rs
+++ core/jubjub/tests/arithmetic.rs
@@ -50,4 +50,36 @@ mod arithmetic_tests {
             assert_eq!(a, c);
         }
     }
+
+    #[test]
+    fn mul_test() {
+        for i in 0..100000 {
+            let mut initial_seeds = [
+                0x43, 0x62, 0xbe, 0x7d, 0x23, 0xad, 0x56, 0xcd, 0x33, 0x0a, 0x22, 0x23, 0x46, 0x36,
+                0xac, 0xef,
+            ];
+            let seed = i as u8 % u8::MAX;
+            let index = (seed % 16) as usize;
+            initial_seeds[index] = seed;
+            let rng = XorShiftRng::from_seed(initial_seeds);
+
+            let mut a = Fr::random(rng.clone());
+            let mut a2 = a.clone();
+            let b = Fr::random(rng.clone());
+            let c = Fr::random(rng);
+            let mut d = a.clone();
+            let mut e = b.clone();
+            let f = c.clone();
+
+            // a * b + a * c = a(b + c)
+            a.mul_assign(b);
+            a2.mul_assign(c);
+            a.add_assign(a2);
+
+            e.add_assign(f);
+            d.mul_assign(e);
+
+            assert_eq!(a, d);
+        }
+    }
 }
