cf5a7bcdb76b89697409637b2a52347d9c12b677
NoCtrlZ <phantomofrotten@gmail.com>
2022-04-03 12:18:25 +0900

    commonize comparison operation and basic trait

--- snarks/jubjub/src/operation.rs
+++ snarks/jubjub/src/operation.rs
@@ -1,5 +1,6 @@
 macro_rules! field_operation {
     ($field:ident) => {
+        // three basic arithmetic
         impl<'a, 'b> Add<&'b $field> for &'a $field {
             type Output = $field;
 
@@ -69,6 +70,91 @@ macro_rules! field_operation {
             #[inline]
             fn neg(self) -> $field {
                 $field(neg(&self.0))
+            }
+        }
+
+        // comparison operation
+        impl Eq for Fr {}
+
+        impl PartialEq for Fr {
+            fn eq(&self, other: &Self) -> bool {
+                self.0[0] == other.0[0]
+                    && self.0[1] == other.0[1]
+                    && self.0[2] == other.0[2]
+                    && self.0[3] == other.0[3]
+            }
+        }
+
+        impl PartialOrd for $field {
+            fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+                Some(self.cmp(other))
+            }
+
+            fn lt(&self, other: &Self) -> bool {
+                for (a, b) in self.0.iter().rev().zip(other.0.iter().rev()) {
+                    if a != b {
+                        return a < b;
+                    }
+                }
+                false
+            }
+
+            fn le(&self, other: &Self) -> bool {
+                for (a, b) in self.0.iter().rev().zip(other.0.iter().rev()) {
+                    if a != b {
+                        return a < b;
+                    }
+                }
+                true
+            }
+
+            fn gt(&self, other: &Self) -> bool {
+                for (a, b) in self.0.iter().rev().zip(other.0.iter().rev()) {
+                    if a != b {
+                        return a > b;
+                    }
+                }
+                false
+            }
+
+            fn ge(&self, other: &Self) -> bool {
+                for (a, b) in self.0.iter().rev().zip(other.0.iter().rev()) {
+                    if a != b {
+                        return a > b;
+                    }
+                }
+                true
+            }
+        }
+
+        impl Ord for Fr {
+            fn cmp(&self, other: &Self) -> Ordering {
+                for (a, b) in self.0.iter().rev().zip(other.0.iter().rev()) {
+                    if a < b {
+                        return Ordering::Less;
+                    } else if a > b {
+                        return Ordering::Greater;
+                    }
+                }
+                Ordering::Equal
+            }
+        }
+
+        // basic trait
+        impl Default for $field {
+            fn default() -> Self {
+                $field::zero()
+            }
+        }
+
+        impl Display for $field {
+            fn fmt(&self, f: &mut Formatter) -> FmtResult {
+                let tmp = self.to_bytes();
+                write!(f, "0x")?;
+                for &b in tmp.iter().rev() {
+                    write!(f, "{:02x}", b)?;
+                }
+                Ok(())
             }
         }
     };
