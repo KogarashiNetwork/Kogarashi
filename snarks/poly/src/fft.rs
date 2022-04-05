f331299bba546b7d88de1ea1172deee6b29babfa
NoCtrlZ <phantomofrotten@gmail.com>
2022-04-06 08:36:01 +0900

    separate bit reverse and butterfly

--- snarks/poly/src/fft.rs
+++ snarks/poly/src/fft.rs
@@ -1,66 +1,24 @@
 use sp_std::{vec, vec::Vec};
 use zero_jubjub::Fr;
 
-pub struct Fft {
+pub struct Butterfly {
     k: usize,
     n: usize,
     twiddles: Vec<Fr>,
-    reversed_indexes: Vec<usize>,
 }
 
-impl Fft {
+impl Butterfly {
     pub fn new(k: usize, omega: Fr) -> Self {
         let n = 1 << k;
-        let mut counter = 2;
-        let mut twiddles = Vec::with_capacity(n / 2);
-        let mut reversed_indexes = vec![0; n];
+        let mut twiddles = vec![Fr::zero(); n / 2];
 
         let mut twiddle = Fr::one();
-        for _ in 0..n / 2 {
-            twiddles.push(twiddle);
+        twiddles.iter_mut().for_each(|value| {
+            *value = twiddle;
             twiddle *= omega;
-        }
+        });
 
-        if k % 2 != 0 {
-            reversed_indexes[0] = 0;
-            reversed_indexes[1] = 1;
-        } else {
-            reversed_indexes[0] = 0;
-            reversed_indexes[1] = 2;
-            reversed_indexes[2] = 1;
-            reversed_indexes[3] = 3;
-            counter *= 2;
-        }
-
-        // calculate bit reverse indexes
-        // Todo: two arrays, swap pair
-        while counter != n {
-            for i in 0..counter {
-                reversed_indexes[i] *= 4;
-                reversed_indexes[i + counter] = reversed_indexes[i] + 2;
-                reversed_indexes[i + 2 * counter] = reversed_indexes[i] + 1;
-                reversed_indexes[i + 3 * counter] = reversed_indexes[i] + 3;
-            }
-            counter *= 4;
-        }
-
-        Fft {
-            k,
-            n,
-            twiddles,
-            reversed_indexes,
-        }
-    }
-
-    fn bit_reverse(&self, coeffs: &mut [Fr]) {
-        let tmp = coeffs.to_vec();
-        for (i, coeff) in tmp.iter().enumerate() {
-            coeffs[self.reversed_indexes[i]] = *coeff;
-        }
-    }
-
-    pub fn fft(&self, coeffs: &mut [Fr]) {
-        self.bit_reverse(coeffs);
+        Butterfly { k, n, twiddles }
     }
 }
 
