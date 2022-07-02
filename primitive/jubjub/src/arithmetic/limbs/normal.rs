98f6c8b5e60741d159fbf7967d829b6a45e80248
NoCtrlZ <phantomofrotten@gmail.com>
2022-07-02 18:02:53 +0900

    fix mac util

--- primitive/jubjub/src/arithmetic/limbs/normal.rs
+++ primitive/jubjub/src/arithmetic/limbs/normal.rs
@@ -44,27 +44,27 @@ pub(crate) const fn double(a: &[u64; 4], p: &[u64; 4]) -> [u64; 4] {
 }
 
 pub(crate) const fn mul(a: &[u64; 4], b: &[u64; 4], p: &[u64; 4]) -> [u64; 4] {
-    let (r0, d) = mac(0, a[0], b[0], 0);
-    let (r1, d) = mac(0, a[0], b[1], d);
-    let (r2, d) = mac(0, a[0], b[2], d);
-    let (r3, d) = mac(0, a[0], b[3], d);
-    let r4 = d;
-    let (r1, d) = mac(r1, a[1], b[0], 0);
-    let (r2, d) = mac(r2, a[1], b[1], d);
-    let (r3, d) = mac(r3, a[1], b[2], d);
-    let (r4, d) = mac(r4, a[1], b[3], d);
-    let r5 = d;
-    let (r2, d) = mac(r2, a[2], b[0], 0);
-    let (r3, d) = mac(r3, a[2], b[1], d);
-    let (r4, d) = mac(r4, a[2], b[2], d);
-    let (r5, d) = mac(r5, a[2], b[3], d);
-    let r6 = d;
-    let (r3, d) = mac(r3, a[3], b[0], 0);
-    let (r4, d) = mac(r4, a[3], b[1], d);
-    let (r5, d) = mac(r5, a[3], b[2], d);
-    let (r6, d) = mac(r6, a[3], b[3], d);
-    let r7 = d;
-    mont(&[r0, r1, r2, r3, r4, r5, r6, r7], p)
+    let (l0, d) = mac(0, a[0], b[0], 0);
+    let (l1, d) = mac(0, a[0], b[1], d);
+    let (l2, d) = mac(0, a[0], b[2], d);
+    let (l3, l4) = mac(0, a[0], b[3], d);
+
+    let (l1, d) = mac(l1, a[1], b[0], 0);
+    let (l2, d) = mac(l2, a[1], b[1], d);
+    let (l3, d) = mac(l3, a[1], b[2], d);
+    let (l4, l5) = mac(l4, a[1], b[3], d);
+
+    let (l2, d) = mac(l2, a[2], b[0], 0);
+    let (l3, d) = mac(l3, a[2], b[1], d);
+    let (l4, d) = mac(l4, a[2], b[2], d);
+    let (l5, l6) = mac(l5, a[2], b[3], d);
+
+    let (l3, d) = mac(l3, a[3], b[0], 0);
+    let (l4, d) = mac(l4, a[3], b[1], d);
+    let (l5, d) = mac(l5, a[3], b[2], d);
+    let (l6, l7) = mac(l6, a[3], b[3], d);
+
+    mont(&[l0, l1, l2, l3, l4, l5, l6, l7], p)
 }
 
 pub(crate) const fn square(a: &[u64; 4], p: &[u64; 4]) -> [u64; 4] {
@@ -108,5 +108,5 @@ pub(crate) const fn mont(a: &[u64; 8], p: &[u64; 4]) -> [u64; 4] {
     let (l6, d) = mac(l6, rhs, p[3], d);
     let (l7, _) = adc(a[7], e, d);
 
-    reduce(&[l4, l5, l6, l7], p)
+    sub(&[l4, l5, l6, l7], p, p)
 }
