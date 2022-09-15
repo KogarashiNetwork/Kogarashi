4610daaeee5b5c041e0f1ea82b702182b003795e
ashWhiteHat <phantomofrotten@gmail.com>
2022-09-15 09:55:04 +0900

    substrate client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use sp_keyring::AccountKeyring;
+use subxt::{ClientBuilder, DefaultConfig, PairSigner, PolkadotExtrinsicParams};
+
+#[subxt::subxt(runtime_metadata_path = "./artifacts/polkadot_metadata.scale")]
+pub mod polkadot {}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let signer = PairSigner::new(AccountKeyring::Alice.pair());
+
+    let api = ClientBuilder::new()
+        .build()
+        .await?
+        .to_runtime_api::<polkadot::RuntimeApi<DefaultConfig, PolkadotExtrinsicParams<DefaultConfig>>>();
+
+    // Submit the `transfer` extrinsic from Alice's account to Bob's.
+    let dest = AccountKeyring::Bob.to_account_id().into();
+
+    // Obtain an extrinsic, calling the "transfer" function in
+    // the "balances" pallet.
+    let extrinsic = api.tx().balances().transfer(dest, 123_456_789_012_345)?;
+
+    // Sign and submit the extrinsic, returning its hash.
+    let tx_hash = extrinsic.sign_and_submit_default(&signer).await?;
+
+    println!("Balance transfer extrinsic submitted: {}", tx_hash);
+
+    Ok(())
 }
