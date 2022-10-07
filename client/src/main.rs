801a8e6425c2acc931133e5ca6b03ed28abe61fe
Kirill Karbushev <kiriosk@yandex.ru>
2022-10-07 15:30:04 +0900

    balance extrinsic implemented

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,47 +1,22 @@
-use codec::Compact;
-use sp_keyring::{sr25519::sr25519::Pair, AccountKeyring};
-use sp_runtime::{AccountId32, MultiAddress};
-use substrate_api_client::{compose_extrinsic, Api, XtStatus};
+use rpc::rpc_to_localhost;
+use sp_keyring::AccountKeyring;
 
-fn main() {
-    let url = "ws://127.0.0.1:9944";
-    let from = AccountKeyring::Alice.pair();
+mod extrinsic;
+mod rpc;
+mod utils;
 
-    let api = Api::<Pair>::new(url.to_string())
-        .map(|api| api.set_signer(from.clone()).unwrap())
+#[tokio::main]
+async fn main() {
+    let xt = extrinsic::create_balance_transfer_xt(
+        AccountKeyring::Alice,
+        AccountKeyring::Bob.to_account_id(),
+        1000,
+    )
+    .await;
+    println!("Extrinsic: {xt}");
+    let res = rpc_to_localhost("author_submitExtrinsic", [xt])
+        .await
         .unwrap();
 
-    let to = AccountKeyring::Bob.to_account_id();
-    match api.get_account_data(&to).unwrap() {
-        Some(bob) => println!("[+] Bob's Free Balance is is {}\n", bob.free),
-        None => println!("[+] Bob's Free Balance is is 0\n"),
-    }
-
-    let xt = compose_extrinsic(
-        &api,
-        "Balances",
-        "transfer",
-        (
-            MultiAddress::<AccountId32, ()>::Id(to.clone()),
-            Compact(1000),
-        ),
-    );
-
-    println!(
-        "Sending an extrinsic from Alice (Key = {:?}),\n\nto Bob (Key = {})\n",
-        from.as_ref().public,
-        to
-    );
-
-    println!("[+] Composed extrinsic: {:?}\n", xt);
-
-    // send and watch extrinsic until finalized
-    let tx_hash = api
-        .send_extrinsic(xt.hex_encode(), XtStatus::InBlock)
-        .unwrap();
-    println!("[+] Transaction got included. Hash: {:?}\n", tx_hash);
-
-    // verify that Bob's free Balance increased
-    let bob = api.get_account_data(&to).unwrap().unwrap();
-    println!("[+] Bob's Free Balance is now {}\n", bob.free);
+    println!("Result: {res}");
 }
