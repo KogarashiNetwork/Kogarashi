94cb405d3061f1faebeec4adf4068557d2fb41cc
ashWhiteHat <phantomofrotten@gmail.com>
2022-11-05 12:10:24 +0900

    init keccak256

--- primitive/hash/src/keccak256.rs
+++ primitive/hash/src/keccak256.rs
@@ -1,3 +1,34 @@
-pub fn keccack256 (input: String) -> String {
-    "".to_string()
+/// keccak implementation according to following
+/// https://keccak.team/obsolete/Keccak-specifications.pdf
+use parity_scale_codec::alloc::vec::Vec;
+
+struct Keccak {
+    r: u32,
+    c: u32,
+    s: Vec<u8>,
+}
+
+impl Keccak {
+    fn new(witdh: usize) -> Self {
+        match witdh {
+            256 => Self {
+                r: 1088,
+                c: 512,
+                s: Vec::new(),
+            },
+            _ => unimplemented!(),
+        }
+    }
+
+    fn pad(input: Vec<u8>) -> Vec<u8> {
+        let n = 8;
+
+        input
+    }
+}
+
+pub fn keccak256(input: Vec<u8>) -> Vec<u8> {
+    let keccak_1600 = Keccak::new(256);
+
+    input
 }
--- primitive/hash/src/lib.rs
+++ primitive/hash/src/lib.rs
@@ -1,10 +1,20 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
 mod keccak256;
+
+pub use keccak256::keccak256;
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        // https://emn178.github.io/online-tools/sha3_256.html
+        let msg = b"hello";
+        let result = b"3338be694f50c5f338814986cdf0686453a888b84f424d792af4b9202398f392".to_vec();
+        let digest = keccak256(msg.to_vec());
+
+        assert_eq!(digest, result);
     }
 }
