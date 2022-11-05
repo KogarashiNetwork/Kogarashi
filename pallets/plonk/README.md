7a0982e5184923b8e24b37409d44f539e754d5d6
ashWhiteHat <phantomofrotten@gmail.com>
2022-11-05 13:50:27 +0900

    add readme description

--- pallets/plonk/README.md
+++ pallets/plonk/README.md
@@ -1,1 +1,14 @@
-License: Unlicense
+# Plonk
+
+## Abstract
+
+We implemented the plonk library as a pallet in order for developers to customize circuits and use the plonk protocol on Substrate runtime. This project allowed us to use following functionalities.
+
+- Custom circuit
+- Trusted setup
+- Generate proof
+- Verify proof
+
+## Acknowledgements
+
+- Initial rust [implementation](https://github.com/ZK-Garage/plonk) of PLONK by ZK-Garage
--- pallets/plonk/src/tests.rs
+++ pallets/plonk/src/tests.rs
@@ -22,68 +22,67 @@ use plonk_core::error::Error;
 use plonk_core::prelude::*;
 use rand_core::OsRng;
 
+// Implements a circuit that checks:
+// 1) a + b = c where C is a PI
+// 2) a <= 2^6
+// 3) b <= 2^4
+// 4) a * b = d where D is a PI
+// 5) JubJub::GENERATOR * e(JubJubScalar) = f where F is a PI
+#[derive(derivative::Derivative)]
+#[derivative(Debug(bound = ""), Default(bound = ""))]
+pub struct TestCircuit<F, P>
+where
+    F: PrimeField,
+    P: TEModelParameters<BaseField = F>,
+{
+    a: F,
+    b: F,
+    c: F,
+    d: F,
+    e: P::ScalarField,
+    f: GroupAffine<P>,
+}
+
+impl<F, P> Circuit<F, P> for TestCircuit<F, P>
+where
+    F: PrimeField,
+    P: TEModelParameters<BaseField = F>,
+{
+    const CIRCUIT_ID: [u8; 32] = [0xff; 32];
+
+    fn gadget(&mut self, composer: &mut StandardComposer<F, P>) -> Result<(), Error> {
+        let a = composer.add_input(self.a);
+        let b = composer.add_input(self.b);
+        let zero = composer.zero_var();
+
+        // Make first constraint a + b = c (as public input)
+        composer.arithmetic_gate(|gate| {
+            gate.witness(a, b, Some(zero))
+                .add(F::one(), F::one())
+                .pi(-self.c)
+        });
+
+        // Check that a and b are in range
+        composer.range_gate(a, 6);
+        composer.range_gate(b, 4);
+        // Make second constraint a * b = d
+        composer.arithmetic_gate(|gate| gate.witness(a, b, Some(zero)).mul(F::one()).pi(-self.d));
+        let e = composer.add_input(from_embedded_curve_scalar::<F, P>(self.e));
+        let (x, y) = P::AFFINE_GENERATOR_COEFFS;
+        let generator = GroupAffine::new(x, y);
+        let scalar_mul_result = composer.fixed_base_scalar_mul(e, generator);
+        // Apply the constrain
+        composer.assert_equal_public_point(scalar_mul_result, self.f);
+        Ok(())
+    }
+
+    fn padded_circuit_size(&self) -> usize {
+        1 << 9
+    }
+}
+
 #[test]
 fn plonk_test() {
-    // Implements a circuit that checks:
-    // 1) a + b = c where C is a PI
-    // 2) a <= 2^6
-    // 3) b <= 2^4
-    // 4) a * b = d where D is a PI
-    // 5) JubJub::GENERATOR * e(JubJubScalar) = f where F is a PI
-    #[derive(derivative::Derivative)]
-    #[derivative(Debug(bound = ""), Default(bound = ""))]
-    pub struct TestCircuit<F, P>
-    where
-        F: PrimeField,
-        P: TEModelParameters<BaseField = F>,
-    {
-        a: F,
-        b: F,
-        c: F,
-        d: F,
-        e: P::ScalarField,
-        f: GroupAffine<P>,
-    }
-
-    impl<F, P> Circuit<F, P> for TestCircuit<F, P>
-    where
-        F: PrimeField,
-        P: TEModelParameters<BaseField = F>,
-    {
-        const CIRCUIT_ID: [u8; 32] = [0xff; 32];
-
-        fn gadget(&mut self, composer: &mut StandardComposer<F, P>) -> Result<(), Error> {
-            let a = composer.add_input(self.a);
-            let b = composer.add_input(self.b);
-            let zero = composer.zero_var();
-
-            // Make first constraint a + b = c (as public input)
-            composer.arithmetic_gate(|gate| {
-                gate.witness(a, b, Some(zero))
-                    .add(F::one(), F::one())
-                    .pi(-self.c)
-            });
-
-            // Check that a and b are in range
-            composer.range_gate(a, 6);
-            composer.range_gate(b, 4);
-            // Make second constraint a * b = d
-            composer
-                .arithmetic_gate(|gate| gate.witness(a, b, Some(zero)).mul(F::one()).pi(-self.d));
-            let e = composer.add_input(from_embedded_curve_scalar::<F, P>(self.e));
-            let (x, y) = P::AFFINE_GENERATOR_COEFFS;
-            let generator = GroupAffine::new(x, y);
-            let scalar_mul_result = composer.fixed_base_scalar_mul(e, generator);
-            // Apply the constrain
-            composer.assert_equal_public_point(scalar_mul_result, self.f);
-            Ok(())
-        }
-
-        fn padded_circuit_size(&self) -> usize {
-            1 << 9
-        }
-    }
-
     // Generate CRS
     type PC = SonicKZG10<Bls12_381, DensePolynomial<BlsScalar>>;
     let pp = PC::setup(1 << 10, None, &mut OsRng)
