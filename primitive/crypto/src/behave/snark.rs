2098180b9faf051883b433f676b19909f03c9616
ashWhiteHat <phantomofrotten@gmail.com>
2022-11-06 17:20:17 +0900

    enable fft only when std

--- primitive/crypto/src/behave/snark.rs
+++ primitive/crypto/src/behave/snark.rs
@@ -1,23 +1,24 @@
 // This trait resresents zkSNARKs trait
 
-use super::{
-    algebra::Ring,
-    comp::{ParallelCmp, ParityCmp},
-    field::PrimeField,
-};
+use super::{comp::ParityCmp, field::PrimeField};
 
-/// This is fft field
-/// This is used for fft and has roots of unity
-pub trait FftField: PrimeField + ParallelCmp {
-    const ROOT_OF_UNITY: [u64; 4];
-}
+#[cfg(feature = "std")]
+use super::{algebra::Ring, comp::ParallelCmp};
 
 /// This is pairing field
 /// This is used for pairing
 pub trait PairingField: PrimeField + ParityCmp {}
 
+/// This is fft field
+/// This is used for fft and has roots of unity
+#[cfg(feature = "std")]
+pub trait FftField: PrimeField + ParallelCmp {
+    const ROOT_OF_UNITY: [u64; 4];
+}
+
 /// This is polynomial
 /// This has fft functionality and represents polynomial ring
+#[cfg(feature = "std")]
 pub trait Polynomial: Ring + ParallelCmp {
     // domain of polynomial
     type Domain: FftField;
