6935172cbeb2c97c97e43fbcb05a5e31785f70c5
ashWhiteHat <phantomofrotten@gmail.com>
2022-11-08 12:39:52 +0900

    define affine and projective trait

--- primitive/crypto/src/behave/curve.rs
+++ primitive/crypto/src/behave/curve.rs
@@ -6,12 +6,38 @@ use super::{algebra::Ring, basic::Basic, comp::ParityCmp, field::PrimeField};
 /// This has add and mul operation
 /// y^2 = x^3 + ax + b
 pub trait Curve: ParityCmp + Ring + Basic {
+    // projective coordinate representation
+    type Projective: Projective;
+
+    // affine coordinate representation
+    type Affine: Affine;
+
     // scalar field of curve
     type ScalarField: PrimeField;
 
     // a param
-    fn const_a() -> Self::ScalarField;
+    const PARAM_A: Self::ScalarField;
 
     // b param
-    fn const_b() -> Self::ScalarField;
+    const PARAM_B: Self::ScalarField;
+
+    // check that point is on curve
+    fn is_on_curve(self, point: Self::Affine) -> bool;
+}
+
+pub trait Affine: ParityCmp + Basic {
+    type Projective: Projective;
+
+    // convert affine to projective representation
+    fn to_projective(self) -> Self::Projective;
+}
+
+pub trait Projective: Ring + Basic {
+    type Affine: Affine;
+
+    // convert projective to affine representation
+    fn to_affine(self) -> Self::Affine;
+
+    // check that point is on curve
+    fn is_identity(self) -> bool;
 }
