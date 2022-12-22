9c6219651173974c9211effc7c17582b3b3bb3d4
Kirill Karbushev <kiriosk@yandex.ru>
2022-12-23 06:37:58 +0900

    fix circuit constraints and variable initialization

--- example/plonk-circuits/src/main.rs
+++ example/plonk-circuits/src/main.rs
@@ -21,17 +21,15 @@ fn main() -> Result<(), Error> {
         P: TEModelParameters<BaseField = F>,
     {
         alice_public_key: GroupAffine<P>,
-        // bob_public_key: GroupAffine<P>,
+        bob_public_key: GroupAffine<P>,
         alice_left_encrypted_balance: GroupAffine<P>,
-        // alice_right_encrypted_balance: GroupAffine<P>,
+        alice_right_encrypted_balance: GroupAffine<P>,
         alice_left_encrypted_transfer_amount: GroupAffine<P>,
-        // alice_right_encrypted_transfer_amount: GroupAffine<P>,
-        // bob_left_encrypted_transfer_amount: GroupAffine<P>,
-        // bob_right_encrypted_transfer_amount: GroupAffine<P>,
-        // generator: GroupAffine<P>,
+        alice_right_encrypted_transfer_amount: GroupAffine<P>,
+        bob_left_encrypted_transfer_amount: GroupAffine<P>,
         alice_private_key: P::ScalarField,
         transfer_amount_b: P::ScalarField,
-        // alice_after_balance: P::ScalarField,
+        alice_after_balance: P::ScalarField,
         randomness: P::ScalarField,
     }
 
@@ -46,28 +44,30 @@ fn main() -> Result<(), Error> {
             let (x, y) = P::AFFINE_GENERATOR_COEFFS;
             let generator = GroupAffine::new(x, y);
 
-            // let alice_left_encrypted_balance =
-            //     composer.add_affine_to_circuit_description(self.alice_left_encrypted_balance);
-            // let alice_right_encrypted_balance =
-            //     composer.add_affine_to_circuit_description(self.alice_right_encrypted_balance);
-            // let alice_left_encrypted_transfer_amount = composer
-            //     .add_affine_to_circuit_description(self.alice_left_encrypted_transfer_amount);
-            // let alice_right_encrypted_transfer_amount = composer
-            //     .add_affine_to_circuit_description(self.alice_right_encrypted_transfer_amount);
+            let alice_public_key = composer.add_affine(self.alice_public_key);
+            let bob_public_key = composer.add_affine(self.bob_public_key);
+            let alice_left_encrypted_balance =
+                composer.add_affine(self.alice_left_encrypted_balance);
+            let alice_right_encrypted_balance =
+                composer.add_affine(self.alice_right_encrypted_balance);
+            let alice_left_encrypted_transfer_amount =
+                composer.add_affine(self.alice_left_encrypted_transfer_amount);
+            let alice_right_encrypted_transfer_amount =
+                composer.add_affine(self.alice_right_encrypted_transfer_amount);
             let alice_private_key =
                 composer.add_input(from_embedded_curve_scalar::<F, P>(self.alice_private_key));
             let transfer_amount_b =
                 composer.add_input(from_embedded_curve_scalar::<F, P>(self.transfer_amount_b));
-            // let alice_after_balance =
-            //     composer.add_input(from_embedded_curve_scalar::<F, P>(self.alice_after_balance));
+            let alice_after_balance =
+                composer.add_input(from_embedded_curve_scalar::<F, P>(self.alice_after_balance));
             let randomness =
                 composer.add_input(from_embedded_curve_scalar::<F, P>(self.randomness));
-            // let one = composer.add_input(F::one());
+            let one = composer.add_input(F::one());
 
             // Alice left encrypted transfer check
             let g_pow_balance = composer.fixed_base_scalar_mul(transfer_amount_b, generator);
             let alice_pk_powered_by_randomness =
-                composer.fixed_base_scalar_mul(randomness, self.alice_public_key);
+                composer.variable_base_scalar_mul(randomness, alice_public_key);
             let left_alice_transfer =
                 composer.point_addition_gate(g_pow_balance, alice_pk_powered_by_randomness);
             composer.assert_equal_public_point(
@@ -76,38 +76,38 @@ fn main() -> Result<(), Error> {
             );
 
             // Bob left encrypted transfer check
-            // let bob_pk_powered_by_randomness =
-            //     composer.fixed_base_scalar_mul(randomness, self.bob_public_key);
-            // let left_bob_transfer =
-            //     composer.point_addition_gate(g_pow_balance, bob_pk_powered_by_randomness);
-            // composer.assert_equal_public_point(
-            //     left_bob_transfer,
-            //     self.bob_left_encrypted_transfer_amount,
-            // );
+            let bob_pk_powered_by_randomness =
+                composer.variable_base_scalar_mul(randomness, bob_public_key);
+            let left_bob_transfer =
+                composer.point_addition_gate(g_pow_balance, bob_pk_powered_by_randomness);
+            composer.assert_equal_public_point(
+                left_bob_transfer,
+                self.bob_left_encrypted_transfer_amount,
+            );
 
             // Alice right encrypted transfer check
-            // let g_pow_randomness = composer.fixed_base_scalar_mul(randomness, generator);
-            // composer.assert_equal_public_point(
-            //     g_pow_randomness,
-            //     self.alice_right_encrypted_transfer_amount,
-            // );
+            let g_pow_randomness = composer.fixed_base_scalar_mul(randomness, generator);
+            composer.assert_equal_public_point(
+                g_pow_randomness,
+                self.alice_right_encrypted_transfer_amount,
+            );
 
             // Alice after balance is correct
-            // let g_pow_after_balance =
-            //     composer.fixed_base_scalar_mul(alice_after_balance, generator);
-            // let alice_left_transfer_neg =
-            //     composer.conditional_point_neg(one, alice_left_encrypted_transfer_amount);
-            // let alice_right_transfer_neg =
-            //     composer.conditional_point_neg(one, alice_right_encrypted_transfer_amount);
-            // let left_after_balance =
-            //     composer.point_addition_gate(alice_left_encrypted_balance, alice_left_transfer_neg);
-            // let right_after_balance = {
-            //     let right_after_balance = composer
-            //         .point_addition_gate(alice_right_encrypted_balance, alice_right_transfer_neg);
-            //     composer.variable_base_scalar_mul(alice_private_key, right_after_balance)
-            // };
-            // let x = composer.point_addition_gate(g_pow_after_balance, right_after_balance);
-            // composer.assert_equal_point(left_after_balance, x);
+            let g_pow_after_balance =
+                composer.fixed_base_scalar_mul(alice_after_balance, generator);
+            let alice_left_transfer_neg =
+                composer.conditional_point_neg(one, alice_left_encrypted_transfer_amount);
+            let alice_right_transfer_neg =
+                composer.conditional_point_neg(one, alice_right_encrypted_transfer_amount);
+            let left_after_balance =
+                composer.point_addition_gate(alice_left_encrypted_balance, alice_left_transfer_neg);
+            let right_after_balance = {
+                let right_after_balance = composer
+                    .point_addition_gate(alice_right_encrypted_balance, alice_right_transfer_neg);
+                composer.variable_base_scalar_mul(alice_private_key, right_after_balance)
+            };
+            let x = composer.point_addition_gate(g_pow_after_balance, right_after_balance);
+            composer.assert_equal_point(left_after_balance, x);
 
             // Public key calculated correctly
             let calculated_pk = composer.fixed_base_scalar_mul(alice_private_key, generator);
@@ -117,7 +117,7 @@ fn main() -> Result<(), Error> {
         }
 
         fn padded_circuit_size(&self) -> usize {
-            1 << 12
+            1 << 13
         }
     }
 
@@ -151,37 +151,34 @@ fn main() -> Result<(), Error> {
     let randomness = JubJubScalar::from(123);
 
     let alice_public_key = AffineCurve::mul(&generator, alice_private_key).into();
-    // let bob_public_key = AffineCurve::mul(&generator, bob_private_key).into();
+    let bob_public_key = AffineCurve::mul(&generator, bob_private_key).into();
 
     let alice_left_encrypted_balance = AffineCurve::mul(&generator, alice_balance)
         + AffineCurve::mul(&alice_public_key, alice_original_randomness);
-    // let alice_right_encrypted_balance =
-    //     AffineCurve::mul(&generator, alice_original_randomness).into();
+    let alice_right_encrypted_balance =
+        AffineCurve::mul(&generator, alice_original_randomness).into();
 
     let alice_left_encrypted_transfer_amount = AffineCurve::mul(&generator, transfer_amount_b)
         + AffineCurve::mul(&alice_public_key, randomness);
-    // let alice_right_encrypted_transfer_amount = AffineCurve::mul(&generator, randomness).into();
+    let alice_right_encrypted_transfer_amount = AffineCurve::mul(&generator, randomness).into();
 
-    // let bob_left_encrypted_transfer_amount = AffineCurve::mul(&generator, transfer_amount_b)
-    //     + AffineCurve::mul(&bob_public_key, randomness);
-    // let bob_right_encrypted_transfer_amount = AffineCurve::mul(&generator, randomness).into();
+    let bob_left_encrypted_transfer_amount = AffineCurve::mul(&generator, transfer_amount_b)
+        + AffineCurve::mul(&bob_public_key, randomness);
 
     // Prover POV
     let (proof, pi) = {
         let mut circuit: ConfidentialTransferCircuit<BlsScalar, JubJubParameters> =
             ConfidentialTransferCircuit {
                 alice_public_key,
-                // bob_public_key,
+                bob_public_key,
                 alice_left_encrypted_balance: alice_left_encrypted_balance.into(),
-                // alice_right_encrypted_balance,
+                alice_right_encrypted_balance,
                 alice_left_encrypted_transfer_amount: alice_left_encrypted_transfer_amount.into(),
-                // alice_right_encrypted_transfer_amount,
-                // bob_left_encrypted_transfer_amount: bob_left_encrypted_transfer_amount.into(),
-                // bob_right_encrypted_transfer_amount,
-                // generator,
+                alice_right_encrypted_transfer_amount,
+                bob_left_encrypted_transfer_amount: bob_left_encrypted_transfer_amount.into(),
                 alice_private_key,
                 transfer_amount_b,
-                // alice_after_balance,
+                alice_after_balance,
                 randomness,
             };
         circuit.gen_proof::<PC>(&pp, pk_p, b"ConfidentialTransfer")
