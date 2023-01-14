040b492938514dc781fc178da7f30b6e0eed6fe1
ashWhiteHat <phantomofrotten@gmail.com>
2023-01-14 16:25:06 +0900

    add twisted edwards curve trait

--- primitive/crypto/src/behave/curve.rs
+++ primitive/crypto/src/behave/curve.rs
@@ -1,6 +1,8 @@
 // This trait resresents elliptic curve and its scalar field
 mod basic;
+mod edwards;
 mod weierstrass;
 
 pub use basic::{Affine, Curve, CurveExtend};
+pub use edwards::{TwistedEdwardsAffine, TwistedEdwardsCurve};
 pub use weierstrass::{Projective, WeierstrassAffine, WeierstrassCurve};
