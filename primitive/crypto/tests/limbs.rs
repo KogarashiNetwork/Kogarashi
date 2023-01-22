4fc0bf2fef5e50811926a5257d05699714fde0b3
ashWhiteHat <phantomofrotten@gmail.com>
2023-01-22 13:02:38 +0900

    replace xor rand from crypto

--- primitive/crypto/tests/limbs.rs
+++ primitive/crypto/tests/limbs.rs
@@ -1,9 +1,7 @@
 mod bls12_381;
 mod jubjub;
 
-use proptest::prelude::*;
-use rand::SeedableRng;
-use rand_xorshift::XorShiftRng;
+use rand_core::OsRng;
 
 #[cfg(test)]
 mod jubjub_limbs_tests {
@@ -11,105 +9,96 @@ mod jubjub_limbs_tests {
     use crate::jubjub::field::*;
     use zero_crypto::arithmetic::bits_256::*;
 
-    prop_compose! {
-        fn arb_jubjub_fr()(bytes in [any::<u8>(); 16]) -> [u64; 4] {
-            random(XorShiftRng::from_seed(bytes))
+    fn arb_jubjub_fr() -> [u64; 4] {
+        random(OsRng)
+    }
+
+    #[test]
+    fn jubjub_field_add_test() {
+        let a = arb_jubjub_fr();
+        let b = a;
+        let c = a;
+
+        // a + a = a * 2
+        let d = add(a, b, MODULUS);
+        let e = double(c, MODULUS);
+        assert_eq!(d, e);
+    }
+
+    #[test]
+    fn jubjub_field_sub_test() {
+        let a = arb_jubjub_fr();
+        let b = a;
+        let c = a;
+        let d = a;
+
+        // a - a = a * 2 - a * 2
+        let e = sub(a, b, MODULUS);
+
+        let cc = double(c, MODULUS);
+        let dd = double(d, MODULUS);
+        let f = sub(cc, dd, MODULUS);
+
+        assert_eq!(e, f);
+    }
+
+    #[test]
+    fn jubjub_field_mul_test() {
+        let a = arb_jubjub_fr();
+        let b = arb_jubjub_fr();
+        let c = arb_jubjub_fr();
+
+        // a * b + a * c
+        let ab = mul(a, b, MODULUS, INV);
+        let ac = mul(a, c, MODULUS, INV);
+        let d = add(ab, ac, MODULUS);
+
+        // a * (b + c)
+        let bc = add(b, c, MODULUS);
+        let e = mul(a, bc, MODULUS, INV);
+
+        assert_eq!(d, e);
+    }
+
+    #[test]
+    fn jubjub_field_square_test() {
+        let a = arb_jubjub_fr();
+        let b = arb_jubjub_fr();
+
+        // (a * a) * (b * b)
+        let aa = mul(a, a, MODULUS, INV);
+        let bb = mul(b, b, MODULUS, INV);
+        let c = mul(aa, bb, MODULUS, INV);
+
+        // a^2 * b^2
+        let aa = square(a, MODULUS, INV);
+        let bb = square(b, MODULUS, INV);
+        let d = mul(aa, bb, MODULUS, INV);
+
+        assert_eq!(c, d);
+    }
+
+    #[test]
+    fn jubjub_field_invert_test() {
+        let a = arb_jubjub_fr();
+        let one = from_raw([1, 0, 0, 0]);
+        let inv = invert(a, sub(zero(), [2, 0, 0, 0], MODULUS), one, MODULUS, INV);
+
+        if let Some(x) = inv {
+            let b = mul(a, x, MODULUS, INV);
+            assert_eq!(b, one)
         }
     }
 
-    proptest! {
-        #![proptest_config(ProptestConfig::with_cases(100000))]
-        #[test]
-        fn jubjub_field_add_test(a in arb_jubjub_fr()) {
-            let b = a;
-            let c = a;
+    #[test]
+    fn jubjub_field_power_test() {
+        let a = arb_jubjub_fr();
+        let one = from_raw([1, 0, 0, 0]);
+        let identity = pow(a, sub(zero(), [1, 0, 0, 0], MODULUS), one, MODULUS, INV);
+        let zero_power = pow(a, [0, 0, 0, 0], one, MODULUS, INV);
 
-            // a + a = a * 2
-            let d = add(a, b, MODULUS);
-            let e = double(c, MODULUS);
-            assert_eq!(d, e);
-        }
-    }
-
-    proptest! {
-        #![proptest_config(ProptestConfig::with_cases(100000))]
-        #[test]
-        fn jubjub_field_sub_test(a in arb_jubjub_fr()) {
-            let b = a;
-            let c = a;
-            let d = a;
-
-            // a - a = a * 2 - a * 2
-            let e = sub(a, b, MODULUS);
-
-            let cc = double(c, MODULUS);
-            let dd = double(d, MODULUS);
-            let f = sub(cc, dd, MODULUS);
-
-            assert_eq!(e, f);
-        }
-    }
-
-    proptest! {
-        #![proptest_config(ProptestConfig::with_cases(10000))]
-        #[test]
-        fn jubjub_field_mul_test(a in arb_jubjub_fr(), b in arb_jubjub_fr(), c in arb_jubjub_fr()) {
-            // a * b + a * c
-            let ab = mul(a, b, MODULUS, INV);
-            let ac = mul(a, c, MODULUS, INV);
-            let d = add(ab, ac, MODULUS);
-
-            // a * (b + c)
-            let bc = add(b, c, MODULUS);
-            let e = mul(a, bc, MODULUS, INV);
-
-            assert_eq!(d, e);
-        }
-    }
-
-    proptest! {
-        #![proptest_config(ProptestConfig::with_cases(10000))]
-        #[test]
-        fn jubjub_field_square_test(a in arb_jubjub_fr(), b in arb_jubjub_fr()) {
-            // (a * a) * (b * b)
-            let aa = mul(a, a, MODULUS, INV);
-            let bb = mul(b, b, MODULUS, INV);
-            let c = mul(aa, bb, MODULUS, INV);
-
-            // a^2 * b^2
-            let aa = square(a, MODULUS, INV);
-            let bb = square(b, MODULUS, INV);
-            let d = mul(aa, bb, MODULUS, INV);
-
-            assert_eq!(c, d);
-        }
-    }
-
-    proptest! {
-        #![proptest_config(ProptestConfig::with_cases(10000))]
-        #[test]
-        fn jubjub_field_invert_test(a in arb_jubjub_fr()) {
-            let one = from_raw([1, 0, 0, 0]);
-            let inv = invert(a, sub(zero(), [2, 0, 0, 0], MODULUS), one, MODULUS, INV);
-
-            if let Some(x) = inv {
-                let b = mul(a, x, MODULUS, INV);
-                assert_eq!(b, one)
-            }
-        }
-    }
-
-    proptest! {
-        #![proptest_config(ProptestConfig::with_cases(10000))]
-        #[test]
-        fn jubjub_field_power_test(a in arb_jubjub_fr()) {
-            let one = from_raw([1, 0, 0, 0]);
-            let identity = pow(a, sub(zero(), [1, 0, 0, 0], MODULUS), one, MODULUS, INV);
-            let zero_power = pow(a, [0, 0, 0, 0], one, MODULUS, INV);
-
-            assert_eq!(one, identity);
-            assert_eq!(one, zero_power);
-        }
+        assert_eq!(one, identity);
+        assert_eq!(one, zero_power);
     }
 }
 
@@ -119,105 +108,102 @@ mod bls12_381_limbs_tests {
     use crate::bls12_381::field::*;
     use zero_crypto::arithmetic::bits_384::*;
 
-    prop_compose! {
-        fn arb_bls12_381_fp()(bytes in [any::<u8>(); 16]) -> [u64; 6] {
-            random(XorShiftRng::from_seed(bytes))
+    fn arb_bls12_381_fp() -> [u64; 6] {
+        random(OsRng)
+    }
+
+    #[test]
+    fn bls12_381_field_add_test() {
+        let a = arb_bls12_381_fp();
+        let b = a;
+        let c = a;
+
+        // a + a = a * 2
+        let d = add(a, b, MODULUS);
+        let e = double(c, MODULUS);
+        assert_eq!(d, e);
+    }
+
+    #[test]
+    fn bls12_381_field_sub_test() {
+        let a = arb_bls12_381_fp();
+        let b = a;
+        let c = a;
+        let d = a;
+
+        // a - a = a * 2 - a * 2
+        let e = sub(a, b, MODULUS);
+
+        let cc = double(c, MODULUS);
+        let dd = double(d, MODULUS);
+        let f = sub(cc, dd, MODULUS);
+
+        assert_eq!(e, f);
+    }
+
+    #[test]
+    fn bls12_381_field_mul_test() {
+        let a = arb_bls12_381_fp();
+        let b = arb_bls12_381_fp();
+        let c = arb_bls12_381_fp();
+
+        // a * b + a * c
+        let ab = mul(a, b, MODULUS, INV);
+        let ac = mul(a, c, MODULUS, INV);
+        let d = add(ab, ac, MODULUS);
+
+        // a * (b + c)
+        let bc = add(b, c, MODULUS);
+        let e = mul(a, bc, MODULUS, INV);
+
+        assert_eq!(d, e);
+    }
+
+    #[test]
+    fn bls12_381_field_square_test() {
+        let a = arb_bls12_381_fp();
+        let b = arb_bls12_381_fp();
+
+        // (a * a) * (b * b)
+        let aa = mul(a, a, MODULUS, INV);
+        let bb = mul(b, b, MODULUS, INV);
+        let c = mul(aa, bb, MODULUS, INV);
+
+        // a^2 * b^2
+        let aa = square(a, MODULUS, INV);
+        let bb = square(b, MODULUS, INV);
+        let d = mul(aa, bb, MODULUS, INV);
+
+        assert_eq!(c, d);
+    }
+
+    #[test]
+    fn bls12_381_field_invert_test() {
+        let a = arb_bls12_381_fp();
+        let one = from_raw([1, 0, 0, 0, 0, 0]);
+        let little_fermat = sub(MODULUS, [2, 0, 0, 0, 0, 0], MODULUS);
+        let inv = invert(a, little_fermat, one, MODULUS, INV);
+
+        if let Some(x) = inv {
+            let b = mul(a, x, MODULUS, INV);
+            assert_eq!(b, one)
         }
     }
 
-    proptest! {
-        #![proptest_config(ProptestConfig::with_cases(100000))]
-        #[test]
-        fn bls12_381_field_add_test(a in arb_bls12_381_fp()) {
-            let b = a;
-            let c = a;
+    #[test]
+    fn bls12_381_field_power_test() {
+        let a = arb_bls12_381_fp();
+        let one = from_raw([1, 0, 0, 0, 0, 0]);
+        let identity = pow(
+            a,
+            sub(zero(), [1, 0, 0, 0, 0, 0], MODULUS),
+            one,
+            MODULUS,
+            INV,
+        );
+        let zero_power = pow(a, [0, 0, 0, 0, 0, 0], one, MODULUS, INV);
 
-            // a + a = a * 2
-            let d = add(a, b, MODULUS);
-            let e = double(c, MODULUS);
-            assert_eq!(d, e);
-        }
-    }
-
-    proptest! {
-        #![proptest_config(ProptestConfig::with_cases(100000))]
-        #[test]
-        fn bls12_381_field_sub_test(a in arb_bls12_381_fp()) {
-            let b = a;
-            let c = a;
-            let d = a;
-
-            // a - a = a * 2 - a * 2
-            let e = sub(a, b, MODULUS);
-
-            let cc = double(c, MODULUS);
-            let dd = double(d, MODULUS);
-            let f = sub(cc, dd, MODULUS);
-
-            assert_eq!(e, f);
-        }
-    }
-
-    proptest! {
-        #![proptest_config(ProptestConfig::with_cases(10000))]
-        #[test]
-        fn bls12_381_field_mul_test(a in arb_bls12_381_fp(), b in arb_bls12_381_fp(), c in arb_bls12_381_fp()) {
-            // a * b + a * c
-            let ab = mul(a, b, MODULUS, INV);
-            let ac = mul(a, c, MODULUS, INV);
-            let d = add(ab, ac, MODULUS);
-
-            // a * (b + c)
-            let bc = add(b, c, MODULUS);
-            let e = mul(a, bc, MODULUS, INV);
-
-            assert_eq!(d, e);
-        }
-    }
-
-    proptest! {
-        #![proptest_config(ProptestConfig::with_cases(10000))]
-        #[test]
-        fn bls12_381_field_square_test(a in arb_bls12_381_fp(), b in arb_bls12_381_fp()) {
-            // (a * a) * (b * b)
-            let aa = mul(a, a, MODULUS, INV);
-            let bb = mul(b, b, MODULUS, INV);
-            let c = mul(aa, bb, MODULUS, INV);
-
-            // a^2 * b^2
-            let aa = square(a, MODULUS, INV);
-            let bb = square(b, MODULUS, INV);
-            let d = mul(aa, bb, MODULUS, INV);
-
-            assert_eq!(c, d);
-        }
-    }
-
-    proptest! {
-        #![proptest_config(ProptestConfig::with_cases(10000))]
-        #[test]
-        fn bls12_381_field_invert_test(a in arb_bls12_381_fp()) {
-            let one = from_raw([1, 0, 0, 0, 0, 0]);
-            let little_fermat = sub(MODULUS, [2, 0, 0, 0, 0, 0], MODULUS);
-            let inv = invert(a, little_fermat, one, MODULUS, INV);
-
-            if let Some(x) = inv {
-                let b = mul(a, x, MODULUS, INV);
-                assert_eq!(b, one)
-            }
-        }
-    }
-
-    proptest! {
-        #![proptest_config(ProptestConfig::with_cases(10000))]
-        #[test]
-        fn bls12_381_field_power_test(a in arb_bls12_381_fp()) {
-            let one = from_raw([1, 0, 0, 0, 0, 0]);
-            let identity = pow(a, sub(zero(), [1, 0, 0, 0, 0, 0], MODULUS), one, MODULUS, INV);
-            let zero_power = pow(a, [0, 0, 0, 0, 0, 0], one, MODULUS, INV);
-
-            assert_eq!(one, identity);
-            assert_eq!(one, zero_power);
-        }
+        assert_eq!(one, identity);
+        assert_eq!(one, zero_power);
     }
 }
--- primitive/elgamal/src/lib.rs
+++ primitive/elgamal/src/lib.rs
@@ -142,104 +142,104 @@ impl ConfidentialTransferPublicInputs for EncryptedNumber {
 
 #[cfg(test)]
 mod tests {
-    use proptest::prelude::*;
-    use rand::SeedableRng;
-    use rand_xorshift::XorShiftRng;
+    use rand::{thread_rng, Rng};
+    use rand_core::OsRng;
     use zero_crypto::behave::*;
     use zero_jubjub::Fp;
 
     use crate::EncryptedNumber;
 
-    prop_compose! {
-        fn arb_fr()(bytes in [any::<u8>(); 16]) -> Fp {
-            Fp::random(XorShiftRng::from_seed(bytes))
-        }
-    }
-    proptest! {
-        #![proptest_config(ProptestConfig::with_cases(25))]
-        #[test]
-        fn test_encrypt_decrypt(priv_k in arb_fr(), random in arb_fr(), balance in any::<u16>()) {
-            let enc_balance = EncryptedNumber::encrypt(priv_k, balance as u32, random);
-
-            let dec_balance = enc_balance.decrypt(priv_k);
-            assert_eq!(dec_balance.unwrap(), balance as u32);
-        }
+    fn arb_fr() -> Fp {
+        Fp::random(OsRng)
     }
 
-    proptest! {
-        #![proptest_config(ProptestConfig::with_cases(25))]
-        #[test]
-        fn test_homomorphic(
-            priv_k in arb_fr(), random1 in arb_fr(), random2 in arb_fr(),
-            balance1 in any::<u16>(), balance2 in any::<u16>()
-        ) {
-            let (balance1, balance2) = if balance1 > balance2 {
-                (balance1 as u32, balance2 as u32)
-            } else {
-                (balance2 as u32, balance1 as u32)
-            };
+    #[test]
+    fn test_encrypt_decrypt() {
+        let priv_k = arb_fr();
+        let random = arb_fr();
+        let balance = thread_rng().gen::<u16>();
+        let enc_balance = EncryptedNumber::encrypt(priv_k, balance as u32, random);
 
-            let enc_balance1 = EncryptedNumber::encrypt(priv_k, balance1, random1);
-            let enc_balance2 = EncryptedNumber::encrypt(priv_k, balance2, random2);
-            let enc_sub = enc_balance1 - enc_balance2;
-            let enc_add = enc_balance1 + enc_balance2;
-
-            let dec_sub = enc_sub.decrypt(priv_k);
-            let dec_add = enc_add.decrypt(priv_k);
-
-            assert_eq!(dec_sub.unwrap(), balance1 - balance2);
-            assert_eq!(dec_add.unwrap(), balance1 + balance2);
-        }
+        let dec_balance = enc_balance.decrypt(priv_k);
+        assert_eq!(dec_balance.unwrap(), balance as u32);
     }
 
-    proptest! {
-        #![proptest_config(ProptestConfig::with_cases(100))]
-        #[test]
-        fn test_elgamal(
-            alice_pk in arb_fr(), bob_pk in arb_fr(), alice_balance in 15..u16::MAX, bob_balance in 10..u16::MAX,
-            transfer_amount in 10..u16::MAX, alice_randomness in 10..u64::MAX, bob_randomness in 10..u64::MAX,
-            alice_transfer_randomness in 10..u64::MAX
-        ) {
-            let (alice_balance, transfer_amount) = if alice_balance > transfer_amount {
-                (alice_balance as u32, transfer_amount as u32)
-            } else {
-                (transfer_amount as u32, alice_balance as u32)
-            };
-            let bob_balance = bob_balance as u32;
+    #[test]
+    fn test_homomorphic() {
+        let priv_k = arb_fr();
+        let random1 = arb_fr();
+        let random2 = arb_fr();
+        let balance1 = thread_rng().gen::<u16>();
+        let balance2 = thread_rng().gen::<u16>();
+        let (balance1, balance2) = if balance1 > balance2 {
+            (balance1 as u32, balance2 as u32)
+        } else {
+            (balance2 as u32, balance1 as u32)
+        };
 
-            // TODO
-            let (alice_randomness, alice_transfer_randomness) = if alice_randomness > alice_transfer_randomness {
+        let enc_balance1 = EncryptedNumber::encrypt(priv_k, balance1, random1);
+        let enc_balance2 = EncryptedNumber::encrypt(priv_k, balance2, random2);
+        let enc_sub = enc_balance1 - enc_balance2;
+        let enc_add = enc_balance1 + enc_balance2;
+
+        let dec_sub = enc_sub.decrypt(priv_k);
+        let dec_add = enc_add.decrypt(priv_k);
+
+        assert_eq!(dec_sub.unwrap(), balance1 - balance2);
+        assert_eq!(dec_add.unwrap(), balance1 + balance2);
+    }
+
+    #[test]
+    fn test_elgamal() {
+        let alice_pk = arb_fr();
+        let bob_pk = arb_fr();
+        let alice_balance = thread_rng().gen::<u16>();
+        let bob_balance = thread_rng().gen::<u16>();
+        let transfer_amount = thread_rng().gen::<u16>();
+        let alice_randomness = thread_rng().gen::<u64>();
+        let bob_randomness = thread_rng().gen::<u64>();
+        let alice_transfer_randomness = thread_rng().gen::<u64>();
+
+        let (alice_balance, transfer_amount) = if alice_balance > transfer_amount {
+            (alice_balance as u32, transfer_amount as u32)
+        } else {
+            (transfer_amount as u32, alice_balance as u32)
+        };
+        let bob_balance = bob_balance as u32;
+
+        // TODO
+        let (alice_randomness, alice_transfer_randomness) =
+            if alice_randomness > alice_transfer_randomness {
                 (alice_randomness, alice_transfer_randomness)
             } else {
                 (alice_transfer_randomness, alice_randomness)
             };
-            let alice_randomness = Fp::from(alice_randomness);
-            let bob_randomness = Fp::from(bob_randomness);
-            let alice_transfer_randomness = Fp::from(alice_transfer_randomness);
+        let alice_randomness = Fp::from(alice_randomness);
+        let bob_randomness = Fp::from(bob_randomness);
+        let alice_transfer_randomness = Fp::from(alice_transfer_randomness);
 
-            let alice_balance_enc = EncryptedNumber::encrypt(alice_pk, alice_balance, alice_randomness);
-            let bob_balance_enc = EncryptedNumber::encrypt(bob_pk, bob_balance, bob_randomness);
+        let alice_balance_enc = EncryptedNumber::encrypt(alice_pk, alice_balance, alice_randomness);
+        let bob_balance_enc = EncryptedNumber::encrypt(bob_pk, bob_balance, bob_randomness);
 
-            let transfer_amount_enc_alice =
-                EncryptedNumber::encrypt(alice_pk, transfer_amount, alice_transfer_randomness);
-            let transfer_amount_enc_bob =
-                EncryptedNumber::encrypt(bob_pk, transfer_amount, alice_transfer_randomness);
+        let transfer_amount_enc_alice =
+            EncryptedNumber::encrypt(alice_pk, transfer_amount, alice_transfer_randomness);
+        let transfer_amount_enc_bob =
+            EncryptedNumber::encrypt(bob_pk, transfer_amount, alice_transfer_randomness);
 
-            let alice_after_balance_enc = alice_balance_enc - transfer_amount_enc_alice;
-            let bob_after_balance_enc = bob_balance_enc + transfer_amount_enc_bob;
+        let alice_after_balance_enc = alice_balance_enc - transfer_amount_enc_alice;
+        let bob_after_balance_enc = bob_balance_enc + transfer_amount_enc_bob;
 
-            let alice_randomness_sum = alice_randomness - alice_transfer_randomness;
-            let bob_randomness_sum = bob_randomness + alice_transfer_randomness;
+        let alice_randomness_sum = alice_randomness - alice_transfer_randomness;
+        let bob_randomness_sum = bob_randomness + alice_transfer_randomness;
 
-            let explicit_alice = alice_balance - transfer_amount;
-            let explicit_bob = bob_balance + transfer_amount;
-            let exp_alice_balance_enc =
-                EncryptedNumber::encrypt(alice_pk, explicit_alice, alice_randomness_sum);
-            let exp_bob_balance_enc =
-                EncryptedNumber::encrypt(bob_pk, explicit_bob, bob_randomness_sum);
+        let explicit_alice = alice_balance - transfer_amount;
+        let explicit_bob = bob_balance + transfer_amount;
+        let exp_alice_balance_enc =
+            EncryptedNumber::encrypt(alice_pk, explicit_alice, alice_randomness_sum);
+        let exp_bob_balance_enc =
+            EncryptedNumber::encrypt(bob_pk, explicit_bob, bob_randomness_sum);
 
-            assert_eq!(exp_alice_balance_enc.t, alice_after_balance_enc.t);
-            assert_eq!(exp_bob_balance_enc, bob_after_balance_enc);
-        }
+        assert_eq!(exp_alice_balance_enc.t, alice_after_balance_enc.t);
+        assert_eq!(exp_bob_balance_enc, bob_after_balance_enc);
     }
 }
