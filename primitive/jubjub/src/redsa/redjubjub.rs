fb54a04071dcd540aaca10e3c5604ae70ad003fb
ashWhiteHat <phantomofrotten@gmail.com>
2023-07-14 15:57:48 +0900

    implement redjubjub sign

--- primitive/jubjub/src/redsa/redjubjub.rs
+++ primitive/jubjub/src/redsa/redjubjub.rs
@@ -1,6 +1,24 @@
 use crate::curve::JubjubExtend;
 use crate::fp::Fp;
+
+use dusk_bytes::Serializable;
+use zero_bls12_381::Fr;
 use zero_crypto::behave::DigitalSig;
+
+use blake2b_simd::Params;
+use rand_core::RngCore;
+
+#[derive(Clone)]
+pub struct Signature {
+    s: [u8; 32],
+    e: [u8; 32],
+}
+
+impl Signature {
+    fn new(s: [u8; 32], e: [u8; 32]) -> Self {
+        Self { s, e }
+    }
+}
 
 #[derive(Clone)]
 pub struct PublicKey(JubjubExtend);
@@ -21,3 +39,26 @@ impl PublicKey {
 
 #[derive(Clone)]
 pub struct SecretKey(Fp);
+
+impl SecretKey {
+    pub fn sign(self, m: &[u8], mut rand: impl RngCore) -> Signature {
+        let mut t = [0u8; 80];
+        rand.fill_bytes(&mut t[..]);
+        let r = hash_to_scalar(&t, m);
+        let R = (JubjubExtend::ADDITIVE_GENERATOR * r).as_bytes();
+        let S = r + hash_to_scalar(&R, m);
+        Signature::new(R, S.to_bytes())
+    }
+}
+
+fn hash_to_scalar(a: &[u8], b: &[u8]) -> Fr {
+    let ret = Params::new()
+        .hash_length(64)
+        .personal(b"FROST_RedJubjubM")
+        .to_state()
+        .update(a)
+        .update(b)
+        .finalize()
+        .as_ref();
+    Fr::from_hash(ret)
+}
