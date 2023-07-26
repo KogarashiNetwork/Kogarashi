6616b14cd8f0dcf7241758a4e6e88ee459951ee9
ashWhiteHat <phantomofrotten@gmail.com>
2023-07-26 18:28:30 +0900

    remove unnecessary lib

--- primitive/r1cs/src/prelude.rs
+++
@@ -1,5 +0,0 @@
-pub use crate::composer::circuit::Circuit;
-pub use crate::composer::compiler::Compiler;
-pub use crate::composer::Composer;
-pub use crate::error::Error;
-pub use bls_12_381::Fr as BlsScalar;
