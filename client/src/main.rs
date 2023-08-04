da10de341dfa27fd2129ce4652354092b48efb33
ashWhiteHat <phantomofrotten@gmail.com>
2023-08-04 14:36:26 +0900

    refactor rpc client

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,7 +3,7 @@ mod rpc;
 mod utils;
 mod wallet;
 
-use rpc::{get_balance, rpc_to_localhost};
+use rpc::{get_balance, transfer};
 use sp_keyring::RedjubjubKeyring as AccountKeyring;
 use std::{thread, time::Duration};
 use wallet::Wallet;
@@ -17,26 +17,15 @@ async fn main() {
     let zane = Wallet::generate();
     let before_balance = get_balance(zane.public()).await;
 
-    // send transaction
-    let encoded_tx = extrinsic::create_balance_transfer_xt(
-        AccountKeyring::Alice,
-        zane.to_account_id(),
-        transfer_amount,
-    )
-    .await;
-    let tx = rpc_to_localhost("author_submitExtrinsic", [encoded_tx]).await;
+    // transfer
+    transfer(AccountKeyring::Alice, zane.to_account_id(), transfer_amount)
+        .await
+        .unwrap();
 
     // wait block inclusion
     thread::sleep(Duration::from_millis(5000));
 
-    match tx {
-        Ok(_) => {
-            // check state transition
-            let after_balance = get_balance(zane.public()).await;
-            assert_eq!(before_balance + transfer_amount, after_balance)
-        }
-        Err(_) => {
-            assert!(false, "transaction failure")
-        }
-    }
+    // check state transition
+    let after_balance = get_balance(zane.public()).await;
+    assert_eq!(before_balance + transfer_amount, after_balance)
 }
--- client/src/rpc.rs
+++ client/src/rpc.rs
@@ -1,3 +1,4 @@
+use crate::extrinsic;
 use crate::utils::{black2_128concat, encoded_key};
 use frame_system::AccountInfo;
 use hex::FromHex;
@@ -5,6 +6,7 @@ use pallet_balances::AccountData;
 use serde_json::{json, Value};
 use sp_core::Decode;
 use sp_core::{redjubjub::Public, H256};
+use sp_keyring::RedjubjubKeyring;
 use sp_runtime::AccountId32;
 use sp_version::RuntimeVersion;
 use std::str::FromStr;
@@ -12,27 +14,6 @@ use std::str::FromStr;
 type AccountMeta = AccountInfo<u32, AccountData<u128>>;
 
 const LOCALHOST_RPC_URL: &str = "http://localhost:9933";
-
-pub async fn rpc_to_localhost<Params: serde::Serialize>(
-    method: &str,
-    params: Params,
-) -> anyhow::Result<Value> {
-    let client = reqwest::Client::new();
-    let mut body: Value = client
-        .post(LOCALHOST_RPC_URL)
-        .json(&json! {{
-            "id": 1,
-            "jsonrpc": "2.0",
-            "method": method,
-            "params": params
-        }})
-        .send()
-        .await?
-        .json()
-        .await?;
-
-    Ok(body["result"].take())
-}
 
 pub async fn get_nonce(account: &AccountId32) -> u32 {
     let nonce_json = rpc_to_localhost("system_accountNextIndex", (account,))
@@ -59,6 +40,15 @@ pub async fn get_runtime_version() -> RuntimeVersion {
     serde_json::from_value(runtime_version_json).unwrap()
 }
 
+pub async fn transfer(
+    from: RedjubjubKeyring,
+    to: AccountId32,
+    amout: u128,
+) -> anyhow::Result<Value> {
+    let encoded_tx = extrinsic::create_balance_transfer_xt(from, to, amout).await;
+    rpc_to_localhost("author_submitExtrinsic", [encoded_tx]).await
+}
+
 async fn get_system_account_info(account: Public) -> AccountMeta {
     let prefix = encoded_key(b"System", b"Account");
     let postfix = black2_128concat(account);
@@ -67,4 +57,25 @@ async fn get_system_account_info(account: Public) -> AccountMeta {
         .unwrap();
     let data = Vec::from_hex(res.as_str().unwrap().replace("0x", "")).unwrap();
     AccountInfo::decode(&mut data.as_slice()).unwrap()
+}
+
+async fn rpc_to_localhost<Params: serde::Serialize>(
+    method: &str,
+    params: Params,
+) -> anyhow::Result<Value> {
+    let client = reqwest::Client::new();
+    let mut body: Value = client
+        .post(LOCALHOST_RPC_URL)
+        .json(&json! {{
+            "id": 1,
+            "jsonrpc": "2.0",
+            "method": method,
+            "params": params
+        }})
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    Ok(body["result"].take())
 }
