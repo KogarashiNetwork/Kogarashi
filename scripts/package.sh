b00ab2f2ae073321a0cbcb84ed99e51a01f704b5
ashWhiteHat <phantomofrotten@gmail.com>
2023-08-05 23:19:16 +0900

    package release script

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,3 +2,5 @@
 
 docker build docker -f docker/LIGHT -t ghcr.io/kogarashinetwork/kogarashi/light
 docker push ghcr.io/kogarashinetwork/kogarashi/light
+docker build . -f docker/FULL -t ghcr.io/kogarashinetwork/kogarashi/full
+docker push ghcr.io/kogarashinetwork/kogarashi/full
--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,4 @@
 
 rustup target add wasm32-unknown-unknown
 cd node && cargo build
-./target/debug/kogarashi-node --dev & cargo test
+./target/debug/kogarashi-node --dev & (cargo test && cargo test --features integration cli_test -- --nocapture)
