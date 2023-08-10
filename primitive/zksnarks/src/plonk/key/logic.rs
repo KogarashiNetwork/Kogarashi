2d3a9f29981f95d7caccaa12689c248849a3cf92
ashWhiteHat <phantomofrotten@gmail.com>
2023-08-10 21:57:58 +0900

    implement logic proving key

--- primitive/zksnarks/src/plonk/key/logic.rs
+++ primitive/zksnarks/src/plonk/key/logic.rs
@@ -1,7 +1,7 @@
 use crate::plonk::proof::Evaluations;
 
-use poly_commit::Commitment;
-use zkstd::common::{vec, Affine, PrimeField, Vec};
+use poly_commit::{Commitment, Evaluations as PolyEval, Polynomial};
+use zkstd::common::{vec, Affine, FftField, PrimeField, Vec};
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct VerificationKey<A: Affine> {
@@ -40,6 +40,92 @@ impl<A: Affine> VerificationKey<A> {
             vec![(c_0 + c_1 + c_2 + c_3 + c_4) * logic_separation_challenge],
             vec![self.q_logic.0],
         )
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct ProvingKey<F: FftField> {
+    pub q_c: (Polynomial<F>, PolyEval<F>),
+    pub q_logic: (Polynomial<F>, PolyEval<F>),
+}
+
+impl<F: FftField> ProvingKey<F> {
+    pub fn compute_quotient_i(
+        &self,
+        index: usize,
+        logic_separation_challenge: &F,
+        a_w_i: &F,
+        a_w_i_next: &F,
+        b_w_i: &F,
+        b_w_i_next: &F,
+        c_w_i: &F,
+        d_w_i: &F,
+        d_w_i_next: &F,
+    ) -> F {
+        let four = F::from(4);
+
+        let q_logic_i = &self.q_logic.1[index];
+        let q_c_i = &self.q_c.1[index];
+
+        let kappa = logic_separation_challenge.square();
+        let kappa_sq = kappa.square();
+        let kappa_cu = kappa_sq * kappa;
+        let kappa_qu = kappa_cu * kappa;
+
+        let a = *a_w_i_next - four * a_w_i;
+        let c_0 = delta(a);
+
+        let b = *b_w_i_next - four * b_w_i;
+        let c_1 = delta(b) * kappa;
+
+        let d = *d_w_i_next - four * d_w_i;
+        let c_2 = delta(d) * kappa_sq;
+
+        let w = c_w_i;
+        let c_3 = (*w - a * b) * kappa_cu;
+
+        let c_4 = delta_xor_and(&a, &b, w, &d, q_c_i) * kappa_qu;
+
+        *q_logic_i * (c_3 + c_0 + c_1 + c_2 + c_4) * logic_separation_challenge
+    }
+
+    pub fn linearize(
+        &self,
+        logic_separation_challenge: &F,
+        a_eval: &F,
+        a_next_eval: &F,
+        b_eval: &F,
+        b_next_eval: &F,
+        c_eval: &F,
+        d_eval: &F,
+        d_next_eval: &F,
+        q_c_eval: &F,
+    ) -> Polynomial<F> {
+        let four = F::from(4);
+        let q_logic_poly = &self.q_logic.0;
+
+        let kappa = logic_separation_challenge.square();
+        let kappa_sq = kappa.square();
+        let kappa_cu = kappa_sq * kappa;
+        let kappa_qu = kappa_cu * kappa;
+
+        let a = *a_next_eval - four * a_eval;
+        let c_0 = delta(a);
+
+        let b = *b_next_eval - four * b_eval;
+        let c_1 = delta(b) * kappa;
+
+        let d = *d_next_eval - four * d_eval;
+        let c_2 = delta(d) * kappa_sq;
+
+        let w = c_eval;
+        let c_3 = (*w - a * b) * kappa_cu;
+
+        let c_4 = delta_xor_and(&a, &b, w, &d, q_c_eval) * kappa_qu;
+
+        let t = (c_0 + c_1 + c_2 + c_3 + c_4) * logic_separation_challenge;
+
+        q_logic_poly * &t
     }
 }
 
