c585148cf57197968d28948dbb4db88501f2a497
ashWhiteHat <phantomofrotten@gmail.com>
2023-08-22 18:57:23 +0900

    weierstrass curve complete double

--- primitive/zkstd/src/arithmetic/points/weierstrass.rs
+++ primitive/zkstd/src/arithmetic/points/weierstrass.rs
@@ -4,6 +4,7 @@ use crate::common::{CurveGroup, PrimeField, Projective, WeierstrassCurve};
 /// weierstrass projective coordinate addition
 #[inline(always)]
 pub fn add_point<P: Projective>(lhs: P, rhs: P) -> P {
+    // Algorithm 7, https://eprint.iacr.org/2015/1060.pdf
     let b3 = <P as WeierstrassCurve>::PARAM_3B;
     let (x0, y0, z0) = (lhs.get_x(), lhs.get_y(), lhs.get_z());
     let (x1, y1, z1) = (rhs.get_x(), rhs.get_y(), rhs.get_z());
@@ -48,21 +49,31 @@ pub fn add_point<P: Projective>(lhs: P, rhs: P) -> P {
 /// weierstrass projective coordinate doubling
 #[inline(always)]
 pub fn double_point<P: Projective>(point: P) -> P {
-    if point.is_identity() || point.get_y().is_zero() {
+    // Algorithm 9, https://eprint.iacr.org/2015/1060.pdf
+    if point.is_identity() {
         <P as CurveGroup>::ADDITIVE_IDENTITY
     } else {
-        let xx = point.get_x().square();
-        let t = xx.double() + xx;
-        let u = (point.get_y() * point.get_z()).double();
-        let v = (u * point.get_x() * point.get_y()).double();
-        let w = t.square() - v.double();
-        let uu = u.square();
+        let b3 = <P as WeierstrassCurve>::PARAM_3B;
+        let (x, y, z) = (point.get_x(), point.get_y(), point.get_z());
 
-        let x = u * w;
-        let y = t * (v - w) - (uu * point.get_y().square()).double();
-        let z = uu * u;
+        let t0 = y.square();
+        let z3 = t0.double().double().double();
+        let t1 = y * z;
+        let t2 = z.square();
+        let t2 = b3 * t2;
+        let x3 = t2 * z3;
+        let y3 = t0 + t2;
+        let z3 = t1 * z3;
+        let t1 = t2.double();
+        let t2 = t1 + t2;
+        let t0 = t0 - t2;
+        let y3 = t0 * y3;
+        let y3 = x3 + y3;
+        let t1 = x * y;
+        let x3 = t0 * t1;
+        let x3 = x3.double();
 
-        P::new(x, y, z)
+        P::new(x3, y3, z3)
     }
 }
 
