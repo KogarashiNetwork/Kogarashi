aac4c06d9c07032655754ebf916bb20a185fa177
ashWhiteHat <phantomofrotten@gmail.com>
2023-08-28 18:07:52 +0900

    zkstd: refactor weierstrass arithmetic

--- primitive/zkstd/src/arithmetic/points/weierstrass.rs
+++ primitive/zkstd/src/arithmetic/points/weierstrass.rs
@@ -4,46 +4,40 @@ use crate::common::{CurveGroup, PrimeField, Projective, WeierstrassCurve};
 /// weierstrass projective coordinate addition
 #[inline(always)]
 pub fn add_point<P: Projective>(lhs: P, rhs: P) -> P {
-    // Algorithm 7, https://eprint.iacr.org/2015/1060.pdf
-    let b3 = <P as WeierstrassCurve>::PARAM_3B;
     let (x0, y0, z0) = (lhs.get_x(), lhs.get_y(), lhs.get_z());
     let (x1, y1, z1) = (rhs.get_x(), rhs.get_y(), rhs.get_z());
 
-    let t0 = x0 * x1;
-    let t1 = y0 * y1;
-    let t2 = z0 * z1;
-    let t3 = x0 + y0;
-    let t4 = x1 + y1;
-    let t3 = t3 * t4;
-    let t4 = t0 + t1;
-    let t3 = t3 - t4;
-    let t4 = y0 + z0;
-    let x3 = y1 + z1;
-    let t4 = t4 * x3;
-    let x3 = t1 + t2;
-    let t4 = t4 - x3;
-    let x3 = x0 + z0;
-    let y3 = x1 + z1;
-    let x3 = x3 * y3;
-    let y3 = t0 + t2;
-    let y3 = x3 - y3;
-    let x3 = t0.double();
-    let t0 = x3 + t0;
-    let t2 = b3 * t2;
-    let z3 = t1 + t2;
-    let t1 = t1 - t2;
-    let y3 = b3 * y3;
-    let x3 = t4 * y3;
-    let t2 = t3 * t1;
-    let x3 = t2 - x3;
-    let y3 = y3 * t0;
-    let t1 = t1 * z3;
-    let y3 = t1 + y3;
-    let t0 = t0 * t3;
-    let z3 = z3 * t4;
-    let z3 = z3 + t0;
+    if lhs.is_identity() {
+        return rhs;
+    } else if rhs.is_identity() {
+        return lhs;
+    }
 
-    P::new(x3, y3, z3)
+    let s1 = y0 * z1;
+    let s2 = y1 * z0;
+    let u1 = x0 * z1;
+    let u2 = x1 * z0;
+
+    if u1 == u2 {
+        if s1 == s2 {
+            return double_point(lhs);
+        } else {
+            return <P as CurveGroup>::ADDITIVE_IDENTITY;
+        }
+    }
+
+    let s = s1 - s2;
+    let u = u1 - u2;
+    let uu = u.square();
+    let v = z0 * z1;
+    let w = s.square() * v - uu * (u1 + u2);
+    let uuu = uu * u;
+
+    let x = u * w;
+    let y = s * (u1 * uu - w) - s1 * uuu;
+    let z = uuu * v;
+
+    P::new(x, y, z)
 }
 
 /// weierstrass projective coordinate doubling
