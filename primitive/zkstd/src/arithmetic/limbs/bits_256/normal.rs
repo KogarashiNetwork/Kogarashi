2d9284e03d871eac81982ca366b92481b7b08db9
ashWhiteHat <phantomofrotten@gmail.com>
2023-09-03 15:51:12 +0900

    zkstd: mont limbs opt

--- primitive/zkstd/src/arithmetic/limbs/bits_256/normal.rs
+++ primitive/zkstd/src/arithmetic/limbs/bits_256/normal.rs
@@ -1,4 +1,4 @@
-use crate::arithmetic::{bits_256::*, utils::*};
+use crate::arithmetic::bits_256::*;
 
 #[inline(always)]
 pub const fn add(a: [u64; 4], b: [u64; 4], p: [u64; 4]) -> [u64; 4] {
@@ -183,36 +183,70 @@ pub const fn neg(a: [u64; 4], p: [u64; 4]) -> [u64; 4] {
 #[inline(always)]
 pub const fn mont(a: [u64; 8], p: [u64; 4], inv: u64) -> [u64; 4] {
     let rhs = a[0].wrapping_mul(inv);
-
-    let d = muladdbackskip(rhs, p[0], a[0]);
-    let (l1, d) = mac(a[1], rhs, p[1], d);
-    let (l2, d) = mac(a[2], rhs, p[2], d);
-    let (l3, d) = mac(a[3], rhs, p[3], d);
-    let (l4, e) = addnc(a[4], d);
+    let s = rhs as u128 * p[0] as u128 + a[0] as u128;
+    let d = (s >> 64) as u64;
+    let s = a[1] as u128 + rhs as u128 * p[1] as u128 + d as u128;
+    let (l1, d) = (s as u64, (s >> 64) as u64);
+    let s = a[2] as u128 + rhs as u128 * p[2] as u128 + d as u128;
+    let (l2, d) = (s as u64, (s >> 64) as u64);
+    let s = a[3] as u128 + rhs as u128 * p[3] as u128 + d as u128;
+    let (l3, d) = (s as u64, (s >> 64) as u64);
+    let s = a[4] as u128 + d as u128;
+    let (l4, e) = (s as u64, (s >> 64) as u64);
 
     let rhs = l1.wrapping_mul(inv);
-
-    let d = muladdbackskip(rhs, p[0], l1);
-    let (l2, d) = mac(l2, rhs, p[1], d);
-    let (l3, d) = mac(l3, rhs, p[2], d);
-    let (l4, d) = mac(l4, rhs, p[3], d);
-    let (l5, e) = adc(a[5], e, d);
+    let s = rhs as u128 * p[0] as u128 + l1 as u128;
+    let d = (s >> 64) as u64;
+    let s = l2 as u128 + rhs as u128 * p[1] as u128 + d as u128;
+    let (l2, d) = (s as u64, (s >> 64) as u64);
+    let s = l3 as u128 + rhs as u128 * p[2] as u128 + d as u128;
+    let (l3, d) = (s as u64, (s >> 64) as u64);
+    let s = l4 as u128 + rhs as u128 * p[3] as u128 + d as u128;
+    let (l4, d) = (s as u64, (s >> 64) as u64);
+    let s = a[5] as u128 + e as u128 + d as u128;
+    let (l5, e) = (s as u64, (s >> 64) as u64);
 
     let rhs = l2.wrapping_mul(inv);
-    let d = muladdbackskip(rhs, p[0], l2);
-    let (l3, d) = mac(l3, rhs, p[1], d);
-    let (l4, d) = mac(l4, rhs, p[2], d);
-    let (l5, d) = mac(l5, rhs, p[3], d);
-    let (l6, e) = adc(a[6], e, d);
+    let s = rhs as u128 * p[0] as u128 + l2 as u128;
+    let d = (s >> 64) as u64;
+    let s = l3 as u128 + rhs as u128 * p[1] as u128 + d as u128;
+    let (l3, d) = (s as u64, (s >> 64) as u64);
+    let s = l4 as u128 + rhs as u128 * p[2] as u128 + d as u128;
+    let (l4, d) = (s as u64, (s >> 64) as u64);
+    let s = l5 as u128 + rhs as u128 * p[3] as u128 + d as u128;
+    let (l5, d) = (s as u64, (s >> 64) as u64);
+    let s = a[6] as u128 + e as u128 + d as u128;
+    let (l6, e) = (s as u64, (s >> 64) as u64);
 
     let rhs = l3.wrapping_mul(inv);
-    let d = muladdbackskip(rhs, p[0], l3);
-    let (l4, d) = mac(l4, rhs, p[1], d);
-    let (l5, d) = mac(l5, rhs, p[2], d);
-    let (l6, d) = mac(l6, rhs, p[3], d);
-    let l7 = adcskip(a[7], e, d);
+    let s = rhs as u128 * p[0] as u128 + l3 as u128;
+    let d = (s >> 64) as u64;
+    let s = l4 as u128 + rhs as u128 * p[1] as u128 + d as u128;
+    let (l4, d) = (s as u64, (s >> 64) as u64);
+    let s = l5 as u128 + rhs as u128 * p[2] as u128 + d as u128;
+    let (l5, d) = (s as u64, (s >> 64) as u64);
+    let s = l6 as u128 + rhs as u128 * p[3] as u128 + d as u128;
+    let (l6, d) = (s as u64, (s >> 64) as u64);
+    let l7 = a[7].wrapping_add(e).wrapping_add(d);
 
-    sub([l4, l5, l6, l7], p, p)
+    let s = (l4 as u128).wrapping_sub(p[0] as u128);
+    let (l0, brw) = (s as u64, (s >> 64) as u64);
+    let s = (l5 as u128).wrapping_sub(p[1] as u128 + (brw >> 63) as u128);
+    let (l1, brw) = (s as u64, (s >> 64) as u64);
+    let s = (l6 as u128).wrapping_sub(p[2] as u128 + (brw >> 63) as u128);
+    let (l2, brw) = (s as u64, (s >> 64) as u64);
+    let s = (l7 as u128).wrapping_sub(p[3] as u128 + (brw >> 63) as u128);
+    let (l3, brw) = (s as u64, (s >> 64) as u64);
+
+    let s = l0 as u128 + (p[0] & brw) as u128;
+    let (l0, c) = (s as u64, (s >> 64) as u64);
+    let s = l1 as u128 + (p[1] & brw) as u128 + c as u128;
+    let (l1, c) = (s as u64, (s >> 64) as u64);
+    let s = l2 as u128 + (p[2] & brw) as u128 + c as u128;
+    let (l2, c) = (s as u64, (s >> 64) as u64);
+    let l3 = l3.wrapping_add(p[3] & brw).wrapping_add(c);
+
+    [l0, l1, l2, l3]
 }
 
 #[inline(always)]
