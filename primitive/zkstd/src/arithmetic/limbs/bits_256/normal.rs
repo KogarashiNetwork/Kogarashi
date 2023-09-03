55ebcbc71694ad5173e39344a9a309de52f0e797
ashWhiteHat <phantomofrotten@gmail.com>
2023-09-03 15:03:43 +0900

    zkstd: sub limbs opt

--- primitive/zkstd/src/arithmetic/limbs/bits_256/normal.rs
+++ primitive/zkstd/src/arithmetic/limbs/bits_256/normal.rs
@@ -15,15 +15,22 @@ pub const fn add(a: [u64; 4], b: [u64; 4], p: [u64; 4]) -> [u64; 4] {
 
 #[inline(always)]
 pub const fn sub(a: [u64; 4], b: [u64; 4], p: [u64; 4]) -> [u64; 4] {
-    let (l0, brw) = sbbnc(a[0], b[0]);
-    let (l1, brw) = sbb(a[1], b[1], brw);
-    let (l2, brw) = sbb(a[2], b[2], brw);
-    let (l3, brw) = sbb(a[3], b[3], brw);
+    let s = (a[0] as u128).wrapping_sub(b[0] as u128);
+    let (l0, brw) = (s as u64, (s >> 64) as u64);
+    let s = (a[1] as u128).wrapping_sub(b[1] as u128 + (brw >> 63) as u128);
+    let (l1, brw) = (s as u64, (s >> 64) as u64);
+    let s = (a[2] as u128).wrapping_sub(b[2] as u128 + (brw >> 63) as u128);
+    let (l2, brw) = (s as u64, (s >> 64) as u64);
+    let s = (a[3] as u128).wrapping_sub(b[3] as u128 + (brw >> 63) as u128);
+    let (l3, brw) = (s as u64, (s >> 64) as u64);
 
-    let (l0, c) = addnc(l0, p[0] & brw);
-    let (l1, c) = adc(l1, p[1] & brw, c);
-    let (l2, c) = adc(l2, p[2] & brw, c);
-    let l3 = adcskip(l3, p[3] & brw, c);
+    let s = l0 as u128 + (p[0] & brw) as u128;
+    let (l0, c) = (s as u64, (s >> 64) as u64);
+    let s = l1 as u128 + (p[1] & brw) as u128 + c as u128;
+    let (l1, c) = (s as u64, (s >> 64) as u64);
+    let s = l2 as u128 + (p[2] & brw) as u128 + c as u128;
+    let (l2, c) = (s as u64, (s >> 64) as u64);
+    let l3 = l3.wrapping_add(p[3] & brw).wrapping_add(c);
 
     [l0, l1, l2, l3]
 }
