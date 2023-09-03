6a73a13bddea371894ca7f5e63ee94ae02ebc87e
ashWhiteHat <phantomofrotten@gmail.com>
2023-09-03 15:05:25 +0900

    zkstd: add limbs rp sub

--- primitive/zkstd/src/arithmetic/limbs/bits_256/normal.rs
+++ primitive/zkstd/src/arithmetic/limbs/bits_256/normal.rs
@@ -10,7 +10,24 @@ pub const fn add(a: [u64; 4], b: [u64; 4], p: [u64; 4]) -> [u64; 4] {
     let (l2, c) = (s as u64, (s >> 64) as u64);
     let l3 = a[3].wrapping_add(b[3]).wrapping_add(c);
 
-    sub([l0, l1, l2, l3], p, p)
+    let s = (l0 as u128).wrapping_sub(p[0] as u128);
+    let (l0, brw) = (s as u64, (s >> 64) as u64);
+    let s = (l1 as u128).wrapping_sub(p[1] as u128 + (brw >> 63) as u128);
+    let (l1, brw) = (s as u64, (s >> 64) as u64);
+    let s = (l2 as u128).wrapping_sub(p[2] as u128 + (brw >> 63) as u128);
+    let (l2, brw) = (s as u64, (s >> 64) as u64);
+    let s = (l3 as u128).wrapping_sub(p[3] as u128 + (brw >> 63) as u128);
+    let (l3, brw) = (s as u64, (s >> 64) as u64);
+
+    let s = l0 as u128 + (p[0] & brw) as u128;
+    let (l0, c) = (s as u64, (s >> 64) as u64);
+    let s = l1 as u128 + (p[1] & brw) as u128 + c as u128;
+    let (l1, c) = (s as u64, (s >> 64) as u64);
+    let s = l2 as u128 + (p[2] & brw) as u128 + c as u128;
+    let (l2, c) = (s as u64, (s >> 64) as u64);
+    let l3 = l3.wrapping_add(p[3] & brw).wrapping_add(c);
+
+    [l0, l1, l2, l3]
 }
 
 #[inline(always)]
