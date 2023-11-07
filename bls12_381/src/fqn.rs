f0e7e6d73d7fb2f5055b5115b34e75de41778703
ashWhiteHat <phantomofrotten@gmail.com>
2023-11-07 19:17:25 +0900

    chore: remove peculiar extension

--- bls12_381/src/fqn.rs
+++ bls12_381/src/fqn.rs
@@ -8,7 +8,7 @@ use crate::params::{
 };
 use zkstd::common::*;
 use zkstd::macros::extension_field::*;
-use zkstd::macros::pairing::{bls12_range_field_pairing, peculiar_extension_field_operation};
+use zkstd::macros::pairing::bls12_range_field_pairing;
 
 // sextic twist of Fp12
 // degree 2 extension field
@@ -116,17 +116,256 @@ impl Fq2 {
     }
 }
 
-// non common extension operation
-peculiar_extension_field_operation!(
-    Fq2,
-    Fq6,
-    Fq12,
-    FROBENIUS_COEFF_FQ2_C1,
-    FROBENIUS_COEFF_FQ6_C1,
-    FROBENIUS_COEFF_FQ6_C2,
-    FROBENIUS_COEFF_FQ12_C1,
-    BLS_X_IS_NEGATIVE
-);
+impl Debug for Fq2 {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{:?} + {:?} u", self.0[0], self.0[1])
+    }
+}
+
+impl Debug for Fq6 {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(
+            f,
+            "{:?} + ({:?}) v + ({:?}) v^2",
+            self.0[0], self.0[1], self.0[2]
+        )
+    }
+}
+
+impl Debug for Fq12 {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{:?} + ({:?}) w", self.0[0], self.0[1])
+    }
+}
+
+impl Fq2 {
+    fn get_invert(self) -> Option<Self> {
+        match self.is_zero() {
+            true => None,
+            _ => {
+                let t = self.0[0].square() + self.0[1].square();
+                let t_inv = t.invert().unwrap();
+                Some(Self([t_inv * self.0[0], t_inv * -self.0[1]]))
+            }
+        }
+    }
+
+    fn mul_ext_field(self, rhs: Self) -> Self {
+        let re = (self.0[0] * rhs.0[0]) - (self.0[1] * rhs.0[1]);
+        let im = (self.0[0] * rhs.0[1]) + (self.0[1] * rhs.0[0]);
+        Self([re, im])
+    }
+
+    fn square_ext_field(self) -> Self {
+        let re = self.0[0].square() - self.0[1].square();
+        let im = (self.0[0] * self.0[1]).double();
+        Self([re, im])
+    }
+
+    fn mul_by_nonres(self) -> Self {
+        Self([self.0[0] - self.0[1], self.0[0] + self.0[1]])
+    }
+
+    fn conjugate(&self) -> Self {
+        Self([self.0[0], -self.0[1]])
+    }
+
+    pub fn frobenius_map(&self) -> Self {
+        self.conjugate()
+    }
+
+    fn frobenius_maps(self, power: usize) -> Self {
+        let c0 = self.0[0];
+        let c1 = self.0[1] * FROBENIUS_COEFF_FQ2_C1[power % 2];
+
+        Self([c0, c1])
+    }
+}
+
+impl Fq6 {
+    fn get_invert(self) -> Option<Self> {
+        let c0 = (self.0[1] * self.0[2]).mul_by_nonresidue();
+        let c0 = self.0[0].square() - c0;
+
+        let c1 = self.0[2].square().mul_by_nonresidue();
+        let c1 = c1 - (self.0[0] * self.0[1]);
+
+        let c2 = self.0[1].square();
+        let c2 = c2 - (self.0[0] * self.0[2]);
+
+        let tmp = ((self.0[1] * c2) + (self.0[2] * c1)).mul_by_nonresidue();
+        let tmp = tmp + (self.0[0] * c0);
+
+        tmp.invert().map(|t| Self([t * c0, t * c1, t * c2]))
+    }
+
+    fn mul_ext_field(self, rhs: Self) -> Self {
+        let a_a = self.0[0] * rhs.0[0];
+        let b_b = self.0[1] * rhs.0[1];
+        let c_c = self.0[2] * rhs.0[2];
+
+        let mut t1 = rhs.0[1] + rhs.0[2];
+        {
+            let tmp = self.0[1] + self.0[2];
+
+            t1 *= tmp;
+            t1 -= b_b;
+            t1 -= c_c;
+            t1 = t1.mul_by_nonresidue();
+            t1 += a_a;
+        }
+
+        let mut t3 = rhs.0[0] + rhs.0[2];
+        {
+            let tmp = self.0[0] + self.0[2];
+
+            t3 *= tmp;
+            t3 -= a_a;
+            t3 += b_b;
+            t3 -= c_c;
+        }
+
+        let mut t2 = rhs.0[0] + rhs.0[1];
+        {
+            let tmp = self.0[0] + self.0[1];
+
+            t2 *= tmp;
+            t2 -= a_a;
+            t2 -= b_b;
+            t2 += c_c.mul_by_nonresidue();
+        }
+
+        Self([t1, t2, t3])
+    }
+
+    fn square_ext_field(self) -> Self {
+        let s0 = self.0[0].square();
+        let ab = self.0[0] * self.0[1];
+        let s1 = ab.double();
+        let mut s2 = self.0[0];
+        s2 -= self.0[1];
+        s2 += self.0[2];
+        s2 = s2.square();
+        let bc = self.0[1] * self.0[2];
+        let s3 = bc.double();
+        let s4 = self.0[2].square();
+
+        let c0 = s3.mul_by_nonresidue() + s0;
+        let c1 = s4.mul_by_nonresidue() + s1;
+        let c2 = s1 + s2 + s3 - s0 - s4;
+
+        Self([c0, c1, c2])
+    }
+
+    fn mul_by_nonres(self) -> Self {
+        Self([self.0[2].mul_by_nonresidue(), self.0[0], self.0[1]])
+    }
+
+    pub fn frobenius_map(&self) -> Self {
+        let c0 = self.0[0].frobenius_map();
+        let c1 = self.0[1].frobenius_map() * FROBENIUS_COEFF_FQ6_C1[1];
+        let c2 = self.0[2].frobenius_map() * FROBENIUS_COEFF_FQ6_C2[1];
+
+        Fq6([c0, c1, c2])
+    }
+
+    fn frobenius_maps(self, power: usize) -> Self {
+        let c0 = self.0[0].frobenius_maps(power);
+        let c1 = self.0[1].frobenius_maps(power) * FROBENIUS_COEFF_FQ6_C1[power % 6];
+        let c2 = self.0[2].frobenius_maps(power) * FROBENIUS_COEFF_FQ6_C2[power % 6];
+
+        Self([c0, c1, c2])
+    }
+
+    pub fn mul_by_1(&self, c1: Fq2) -> Self {
+        Self([
+            (self.0[2] * c1).mul_by_nonresidue(),
+            self.0[0] * c1,
+            self.0[1] * c1,
+        ])
+    }
+
+    pub fn mul_by_01(&self, c0: Fq2, c1: Fq2) -> Self {
+        let a_a = self.0[0] * c0;
+        let b_b = self.0[1] * c1;
+        let t1 = (self.0[2] * c1).mul_by_nonresidue() + a_a;
+        let t2 = (c0 + c1) * (self.0[0] + self.0[1]) - a_a - b_b;
+        let t3 = self.0[2] * c0 + b_b;
+
+        Self([t1, t2, t3])
+    }
+}
+
+impl Fq12 {
+    fn get_invert(self) -> Option<Self> {
+        (self.0[0].square() - self.0[1].square().mul_by_nonresidue())
+            .invert()
+            .map(|t| Self([self.0[0] * t, self.0[1] * -t]))
+    }
+
+    fn mul_ext_field(self, rhs: Self) -> Self {
+        let aa = self.0[0] * rhs.0[0];
+        let bb = self.0[1] * rhs.0[1];
+        let o = rhs.0[0] + rhs.0[1];
+        let c1 = self.0[1] + self.0[0];
+        let c1 = c1 * o;
+        let c1 = c1 - aa;
+        let c1 = c1 - bb;
+        let c0 = bb.mul_by_nonresidue();
+        let c0 = c0 + aa;
+
+        Self([c0, c1])
+    }
+
+    fn square_ext_field(self) -> Self {
+        let ab = self.0[0] * self.0[1];
+        let c0c1 = self.0[0] + self.0[1];
+        let c0 = self.0[1].mul_by_nonresidue() + self.0[0];
+        let tmp = c0 * c0c1 - ab;
+
+        Self([tmp - ab.mul_by_nonresidue(), ab.double()])
+    }
+
+    fn mul_by_nonres(self) -> Self {
+        unimplemented!()
+    }
+
+    pub fn conjugate(self) -> Self {
+        Self([self.0[0], -self.0[1]])
+    }
+
+    pub fn frobenius_map(self) -> Self {
+        let c0 = self.0[0].frobenius_map();
+        let c1 =
+            self.0[1].frobenius_map() * Fq6([FROBENIUS_COEFF_FQ12_C1[1], Fq2::zero(), Fq2::zero()]);
+
+        Self([c0, c1])
+    }
+
+    fn frobenius_maps(self, power: usize) -> Self {
+        let c0 = self.0[0].frobenius_maps(power);
+        let c1 = self.0[1].frobenius_maps(power);
+        let c1 = Fq6([
+            c1.0[0] * FROBENIUS_COEFF_FQ12_C1[power % 12],
+            c1.0[1] * FROBENIUS_COEFF_FQ12_C1[power % 12],
+            c1.0[2] * FROBENIUS_COEFF_FQ12_C1[power % 12],
+        ]);
+
+        Self([c0, c1])
+    }
+
+    fn mul_by_014(self, c0: Fq2, c1: Fq2, c4: Fq2) -> Self {
+        let aa = self.0[0].mul_by_01(c0, c1);
+        let bb = self.0[1].mul_by_1(c4);
+        let o = c1 + c4;
+        let c1 = self.0[1] + self.0[0];
+        let c1 = c1.mul_by_01(c0, o);
+        let c0 = bb;
+        let c0 = c0.mul_by_nonresidue();
+
+        Self([c0 + aa, c1 - aa - bb])
+    }
+}
 
 #[cfg(test)]
 mod tests {
