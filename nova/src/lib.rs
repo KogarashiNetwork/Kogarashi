a9b74ae5c26b05538856930bbbff8faf15bf2656
ashWhiteHat <phantomofrotten@gmail.com>
2023-11-10 12:54:36 +0900

    feat: pedersen commit scheme

--- nova/src/lib.rs
+++ nova/src/lib.rs
@@ -1,3 +1,4 @@
 #![doc = include_str!("../README.md")]
 
+mod pedersen;
 mod relaxed_r1cs;
---
+++ nova/src/pedersen.rs
@@ -0,0 +1,23 @@
+use r1cs::DenseVectors;
+use zkstd::common::{BNAffine, Group, RngCore};
+
+pub struct PedersenCommitment<C: BNAffine> {
+    h: C,
+    g: Vec<C>,
+}
+
+impl<C: BNAffine> PedersenCommitment<C> {
+    pub(crate) fn new(n: u64, mut r: impl RngCore) -> Self {
+        let h = C::random(&mut r).into();
+        let g = (0..=1 << n).map(|_| C::random(&mut r).into()).collect();
+        Self { h, g }
+    }
+
+    pub(crate) fn commit(&self, m: &DenseVectors<C::Scalar>, r: &C::Scalar) -> C {
+        (self.h * r
+            + m.iter()
+                .zip(self.g.iter())
+                .fold(C::Extended::ADDITIVE_IDENTITY, |sum, (v, e)| sum + *e * v))
+        .into()
+    }
+}
