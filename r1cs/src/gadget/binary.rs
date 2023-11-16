ecf162e9a8b8b3828e15bc864ce34a60f1a40921
Kirill Karbushev <kiriosk@yandex.ru>
2023-11-16 15:37:31 +0900

    range check

--- r1cs/src/gadget/binary.rs
+++ r1cs/src/gadget/binary.rs
@@ -46,6 +46,26 @@ impl<C: CircuitDriver> BinaryAssignment<C> {
         decomposition
     }
 
+    fn range_check(cs: &mut R1cs<C>, value: &FieldAssignment<C>, num_of_bits: u8) {
+        fn inner_product(cs: &mut R1cs<C>, a: &[u8], b: &[C::Scalar]) -> FieldAssignment<C> {
+            if a.len() == 1 && b.len() == 1 {
+                FieldAssignment::from(BinaryAssignment::witness(cs, a[0]))
+                    * FieldAssignment::constant(b[0])
+            } else {
+                inner_product(cs, &a[0..a.len() / 2], &b[0..b.len() / 2])
+                    + inner_product(cs, &a[a.len() / 2..], &b[b.len() / 2..])
+            }
+        }
+
+        let powers_of_2 = (0..num_of_bits)
+            .rev()
+            .map(|i| C::Scalar::pow_of_2(i as u64))
+            .collect::<Vec<_>>();
+        let mut bits = value.inner().evaluate(&cs.x, &cs.w).to_bits();
+        bits.resize(powers_of_2.len(), 0);
+        FieldAssignment::eq(cs, value, &inner_product(cs, &bits, &powers_of_2));
+    }
+
     pub fn inner(&self) -> &Wire {
         &self.0
     }
